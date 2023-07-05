b5f656298eced9d7afad248cb3963ef36ecda7e8
Jerzy Wilczek <72213407+jerzywilczek@users.noreply.github.com>
2023-07-05 12:59:42 +0200

    Add common crate (#10)
    * Add `compositor_common` crate with a skeleton scene.
    
    For now I opted for every scene node to have a manually specified
    resolution. I think we can add other ways of specifying resolution later
    on.
    
    * Add custom parameters to the transformation.
    
    These are held as `Box<dyn Any>` for now, but this might change in the
    future.
    
    * Change `Rc`s to `Arc`s

---
+++ compositor_common/src/scene.rs
@@ -0,0 +1,37 @@
+use std::{any::Any, collections::HashMap, sync::Arc};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct VideoId(usize);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TransformationRegistryKey(pub String);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Resolution {
+    width: usize,
+    height: usize,
+}
+
+#[derive(Debug)]
+pub enum Node {
+    Video {
+        id: VideoId,
+        resolution: Resolution,
+    },
+
+    Image {
+        data: Vec<u8>,
+        resolution: Resolution,
+    },
+
+    Transformation {
+        registry_key: TransformationRegistryKey,
+        inputs: HashMap<String, Arc<Node>>,
+        resolution: Resolution,
+        params: Box<dyn Any>,
+    },
+}
+
+pub struct Scene {
+    pub final_nodes: Vec<Arc<Node>>,
+}
