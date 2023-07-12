28e6bc56a763137a1e33fea7d20d3684d72aacc4
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-07-12 13:19:08 +0200

    Ensure pts starts from zero

--- src/rtp_receiver.rs
+++ src/rtp_receiver.rs
@@ -36,7 +36,6 @@ impl RtpReceiver {
                     o=- 0 0 IN IP4 127.0.0.1\n\
                     s=No Name\n\
                     c=IN IP4 127.0.0.1\n\
-                    t=0 0\n\
                     m=video {} RTP/AVP 96\n\
                     a=rtpmap:96 H264/90000\n\
                     a=fmtp:96 packetization-mode=1\n\
@@ -60,15 +59,17 @@ impl RtpReceiver {
         let h264_codec = codec::decoder::find(Id::H264).unwrap();
         let mut decoder = decoder.open_as(h264_codec)?;
 
+        let mut pts_offset: Option<i64> = None;
         let mut decoded_frame = frame::Video::empty();
         for (stream, packet) in input_ctx.packets() {
             if stream.index() != input_index {
                 eprintln!("Received frame from unknown stream, skipping");
                 continue;
             }
+
             decoder.send_packet(&packet)?;
             while decoder.receive_frame(&mut decoded_frame).is_ok() {
-                let frame = match frame_from_av(&mut decoded_frame) {
+                let frame = match frame_from_av(&mut decoded_frame, &mut pts_offset) {
                     Ok(frame) => frame,
                     Err(err) => {
                         eprintln!("Dropping frame: {err}");
@@ -83,11 +84,15 @@ impl RtpReceiver {
     }
 }
 
-fn frame_from_av(decoded: &mut Video) -> Result<Frame> {
+fn frame_from_av(decoded: &mut Video, pts_offset: &mut Option<i64>) -> Result<Frame> {
     if decoded.format() != format::pixel::Pixel::YUV420P {
         return Err(anyhow!("only YUV420P is supported"));
     }
-    eprintln!("{:?}", decoded.pts());
+    let original_pts = decoded.pts();
+    if let (Some(pts), None) = (decoded.pts(), &pts_offset) {
+        *pts_offset = Some(-pts)
+    }
+    let pts = original_pts.map(|original_pts| original_pts + pts_offset.unwrap_or(0));
     Ok(Frame {
         data: YuvData {
             y_plane: bytes::Bytes::copy_from_slice(decoded.data(0)),
@@ -98,6 +103,6 @@ fn frame_from_av(decoded: &mut Video) -> Result<Frame> {
             width: decoded.width().try_into()?,
             height: decoded.height().try_into()?,
         },
-        pts: decoded.pts().ok_or_else(|| anyhow!("missing pts"))?,
+        pts: pts.ok_or_else(|| anyhow!("missing pts"))?,
     })
 }
