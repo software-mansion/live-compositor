6b71647c533e45fa5f0e9b7fd2d474ead4769009
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2023-07-13 16:27:21 +0200

    An initial `render` method.
    This patch adds a `Renderer::render` and a `Renderer::update_scene`
    methods. Those are stubs for now (the `render` method just passes one of
    the source frames as the output)

--- compositor_common/src/frame.rs
+++ compositor_common/src/frame.rs
@@ -1,12 +1,14 @@
 use crate::scene::Resolution;
 
 #[allow(dead_code)]
+#[derive(Debug, Clone)]
 pub struct Frame {
     pub data: YuvData,
     pub resolution: Resolution,
     pub pts: i64,
 }
 
+#[derive(Debug, Clone)]
 pub struct YuvData {
     pub y_plane: bytes::Bytes,
     pub u_plane: bytes::Bytes,
