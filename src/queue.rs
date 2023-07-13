b81a0f93eb42186341f18e56cad66a6e5b937c27
Wojciech Barczynski <barczynski.w@gmail.com>
2023-07-13 13:41:49 +0200

    Reformat channel

--- src/queue.rs
+++ src/queue.rs
@@ -55,23 +55,20 @@ impl FramesBatch {
 
 pub struct Queue {
     internal_queue: Arc<Mutex<InternalQueue>>,
-    check_queue_sender: Sender<()>,
-    check_queue_receiver: Receiver<()>,
+    check_queue_channel: (Sender<()>, Receiver<()>),
     output_framerate: Framerate,
-    frames_batches_sent: u32,
+    send_batches_counter: u32,
     time_buffer_duration: Duration,
 }
 
 impl Queue {
     #[allow(dead_code)]
     pub fn new(output_framerate: Framerate) -> Self {
-        let (check_queue_sender, check_queue_receiver) = unbounded();
         Queue {
             internal_queue: Arc::new(Mutex::new(InternalQueue::new())),
-            check_queue_sender,
-            check_queue_receiver,
+            check_queue_channel: unbounded(),
             output_framerate,
-            frames_batches_sent: 0,
+            send_batches_counter: 0,
             time_buffer_duration: Duration::from_millis(100),
         }
     }
@@ -95,7 +92,7 @@ impl Queue {
         let frame_interval_duration = self.output_framerate.get_interval_duration();
         let ticker = tick(frame_interval_duration);
 
-        let check_queue_sender = self.check_queue_sender.clone();
+        let check_queue_sender = self.check_queue_channel.0.clone();
         thread::spawn(move || loop {
             ticker.recv().unwrap();
             check_queue_sender.send(()).unwrap();
@@ -103,7 +100,7 @@ impl Queue {
 
         let start = Instant::now();
 
-        let check_queue_receiver = self.check_queue_receiver.clone();
+        let check_queue_receiver = self.check_queue_channel.1.clone();
         // Checking queue
         thread::spawn(move || loop {
             check_queue_receiver.recv().unwrap();
@@ -111,7 +108,7 @@ impl Queue {
             let mut internal_queue = self.internal_queue.lock().unwrap();
             let buffer_pts = self.get_next_output_buffer_pts();
             let next_buffer_time = self.output_framerate.get_interval_duration()
-                * self.frames_batches_sent
+                * self.send_batches_counter
                 + self.time_buffer_duration;
 
             if start.elapsed() > next_buffer_time
@@ -119,7 +116,7 @@ impl Queue {
             {
                 let frames_batch = internal_queue.get_frames_batch(buffer_pts);
                 sender.send(frames_batch).unwrap();
-                self.frames_batches_sent += 1;
+                self.send_batches_counter += 1;
 
                 internal_queue.drop_useless_frames(self.get_next_output_buffer_pts());
             }
@@ -133,14 +130,14 @@ impl Queue {
         internal_queue.enqueue_frame(input_id, frame)?;
         internal_queue.drop_pad_useless_frames(input_id, self.get_next_output_buffer_pts())?;
 
-        self.check_queue_sender.send(()).unwrap();
+        self.check_queue_channel.0.send(()).unwrap();
 
         Ok(())
     }
 
     fn get_next_output_buffer_pts(&self) -> Pts {
         let nanoseconds_in_second = 1_000_000_000;
-        (nanoseconds_in_second * (self.frames_batches_sent as u64 + 1))
+        (nanoseconds_in_second * (self.send_batches_counter as u64 + 1))
             / self.output_framerate.0 as u64
     }
 }
