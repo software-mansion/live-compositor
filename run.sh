f0b2b5567a1abaea375961fbdc5c196012f30390
Mikołaj Radkowski <mikolaj.radkowski@protonmail.com>
2023-08-01 11:58:26 +0200

    Download CEF automatically

--- run.sh
+++ run.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+target="target/debug"
+if [[ "$1" == "--release" || "$1" == "-r" ]]; then
+    target="target/release"
+fi
+
+
+# Download CEF
+if [[ -z "$CEF_ROOT" || ! -d "$CEF_ROOT" ]]; then
+    export CEF_ROOT=$(pwd)/$target/cef_root
+    
+    if [[ ! -d "$CEF_ROOT" ]]; then
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            if [[ "uname -m" == "x86_64" ]]; then
+                platform="macosx64"
+            else
+                platform="macosarm64"
+            fi
+        elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+            platform="linux64"
+        else
+            echo "Unsupported platform"
+            exit 1
+        fi
+
+        echo "Downloading CEF..."
+        wget https://cef-builds.spotifycdn.com/cef_binary_115.3.11%2Bga61da9b%2Bchromium-115.0.5790.114_$platform.tar.bz2 -O $target/cef.tar.bz2
+        mkdir $target/cef_root
+
+        echo "Extracting..."
+        tar -xf $target/cef.tar.bz2 -C $target/cef_root --strip-components=1
+    fi
+fi
+
 function create_helper_apple_bundle {
     local name=$1
     local bundle_id=$2
@@ -17,10 +50,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # Build compositor
     if ! cargo build "$1" --all; then
         exit 1
-    fi
-    target="target/debug"
-    if [[ "$1" == "--release" || "$1" == "-r" ]]; then
-        target="target/release"
     fi
 
     # Create a bundle
