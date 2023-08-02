6c4513d78f4c7055cb5b2d0a770ac2b72919cd6f
Wojciech Kozyra <wkozyra95@gmail.com>
2023-08-02 10:41:02 +0200

    Refactor texture code + add rescaling (#38)

--- compositor_render/src/renderer/texture.rs
+++ compositor_render/src/renderer/texture.rs
@@ -1,188 +1,34 @@
-use std::{io::Write, sync::Arc};
+use std::{
+    io::Write,
+    mem,
+    ops::DerefMut,
+    sync::{Arc, Mutex},
+};
 
 use bytes::{BufMut, Bytes, BytesMut};
-use compositor_common::{frame::YuvData, scene::Resolution, Frame};
-use wgpu::MapMode;
+use compositor_common::{scene::Resolution, Frame};
+use crossbeam_channel::bounded;
+use log::error;
+use wgpu::{Buffer, BufferAsyncError, MapMode};
+
+use self::{utils::pad_to_256, yuv::YUVPendingDownload};
 
 use super::WgpuCtx;
 
-pub struct Texture {
-    pub texture: wgpu::Texture,
-    pub view: wgpu::TextureView,
-}
+mod base;
+mod rgba;
+mod utils;
+mod yuv;
 
-impl Texture {
-    pub const DEFAULT_BINDING_TYPE: wgpu::BindingType = wgpu::BindingType::Texture {
-        sample_type: wgpu::TextureSampleType::Float { filterable: true },
-        view_dimension: wgpu::TextureViewDimension::D2,
-        multisampled: false,
-    };
-    pub fn new(
-        ctx: &WgpuCtx,
-        label: Option<&str>,
-        size: wgpu::Extent3d,
-        format: wgpu::TextureFormat,
-        usage: wgpu::TextureUsages,
-    ) -> Self {
-        let texture = ctx.device.create_texture(&wgpu::TextureDescriptor {
-            label,
-            size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format,
-            usage,
-            view_formats: &[format],
-        });
+pub type RGBATexture = rgba::RGBATexture;
+pub type YUVTextures = yuv::YUVTextures;
 
-        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-        Self { texture, view }
-    }
-
-    pub fn size(&self) -> wgpu::Extent3d {
-        self.texture.size()
-    }
-
-    pub fn upload_frame_to_textures(ctx: &WgpuCtx, textures: &[Texture; 3], frame: Arc<Frame>) {
-        // TODO maybe resize?
-        textures[0].upload_data(&ctx.queue, &frame.data.y_plane, 1);
-        textures[1].upload_data(&ctx.queue, &frame.data.u_plane, 1);
-        textures[2].upload_data(&ctx.queue, &frame.data.v_plane, 1);
-        // TODO: https://github.com/membraneframework/video_compositor/pull/30#discussion_r1277993507
-        ctx.queue.submit([]);
-    }
-
-    fn upload_data(&self, queue: &wgpu::Queue, data: &[u8], bytes_per_pixel: u32) {
-        queue.write_texture(
-            wgpu::ImageCopyTexture {
-                aspect: wgpu::TextureAspect::All,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                texture: &self.texture,
-            },
-            data,
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(self.texture.width() * bytes_per_pixel),
-                rows_per_image: Some(self.texture.height()),
-            },
-            self.texture.size(),
-        );
-    }
-}
-
-pub struct YUVTextures(pub [Texture; 3]);
-
-impl YUVTextures {
-    pub fn new(ctx: &WgpuCtx, resolution: &Resolution) -> Self {
-        Self([
-            Self::new_plane(ctx, resolution.width, resolution.height),
-            Self::new_plane(ctx, resolution.width / 2, resolution.height / 2),
-            Self::new_plane(ctx, resolution.width / 2, resolution.height / 2),
-        ])
-    }
-
-    fn new_plane(ctx: &WgpuCtx, width: usize, height: usize) -> Texture {
-        Texture::new(
-            ctx,
-            None,
-            wgpu::Extent3d {
-                width: width as u32,
-                height: height as u32,
-                depth_or_array_layers: 1,
-            },
-            wgpu::TextureFormat::R8Unorm,
-            wgpu::TextureUsages::RENDER_ATTACHMENT
-                | wgpu::TextureUsages::TEXTURE_BINDING
-                | wgpu::TextureUsages::COPY_DST
-                | wgpu::TextureUsages::COPY_SRC
-                | wgpu::TextureUsages::TEXTURE_BINDING,
-        )
-    }
-
-    pub fn new_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
-        let create_entry = |binding: u32| wgpu::BindGroupLayoutEntry {
-            binding,
-            ty: Texture::DEFAULT_BINDING_TYPE,
-            visibility: wgpu::ShaderStages::FRAGMENT,
-            count: None,
-        };
-        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            label: Some("yuv all textures bind group layout"),
-            entries: &[create_entry(0), create_entry(1), create_entry(2)],
-        })
-    }
-
-    fn new_bind_group(&self, ctx: &WgpuCtx, layout: &wgpu::BindGroupLayout) -> wgpu::BindGroup {
-        ctx.device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("yuv all textures bind group"),
-            layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&self.0[0].view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::TextureView(&self.0[1].view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 2,
-                    resource: wgpu::BindingResource::TextureView(&self.0[2].view),
-                },
-            ],
-        })
-    }
-}
-
-pub struct RGBATexture(pub Texture);
-
-impl RGBATexture {
-    pub fn new(ctx: &WgpuCtx, resolution: &Resolution) -> Self {
-        Self(Texture::new(
-            ctx,
-            None,
-            wgpu::Extent3d {
-                width: resolution.width as u32,
-                height: resolution.height as u32,
-                depth_or_array_layers: 1,
-            },
-            wgpu::TextureFormat::Rgba8Unorm,
-            wgpu::TextureUsages::RENDER_ATTACHMENT
-                | wgpu::TextureUsages::TEXTURE_BINDING
-                | wgpu::TextureUsages::COPY_DST
-                | wgpu::TextureUsages::TEXTURE_BINDING,
-        ))
-    }
-
-    pub fn new_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
-        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            label: Some("single texture bind group layout"),
-            entries: &[wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                ty: Texture::DEFAULT_BINDING_TYPE,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                count: None,
-            }],
-        })
-    }
-
-    fn new_bind_group(&self, ctx: &WgpuCtx, layout: &wgpu::BindGroupLayout) -> wgpu::BindGroup {
-        ctx.device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("texture bind group"),
-            layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: wgpu::BindingResource::TextureView(&self.0.view),
-            }],
-        })
-    }
-}
+pub type Texture = base::Texture;
 
 pub struct InputTexture {
-    pub textures: YUVTextures,
-    pub bind_group: wgpu::BindGroup,
+    textures: YUVTextures,
+    bind_group: wgpu::BindGroup,
+    resolution: Resolution,
 }
 
 impl InputTexture {
@@ -193,158 +39,146 @@ impl InputTexture {
         Self {
             textures,
             bind_group,
+            resolution: *resolution,
         }
+    }
+
+    pub fn upload(&mut self, ctx: &WgpuCtx, frame: Arc<Frame>) {
+        if frame.resolution != self.resolution {
+            self.textures = YUVTextures::new(ctx, &frame.resolution);
+            self.bind_group = self
+                .textures
+                .new_bind_group(ctx, &ctx.yuv_bind_group_layout);
+            self.resolution = frame.resolution;
+        }
+        self.textures.upload(ctx, &frame.data)
+    }
+
+    pub fn yuv_textures(&self) -> &YUVTextures {
+        &self.textures
+    }
+
+    pub fn bind_group(&self) -> &wgpu::BindGroup {
+        &self.bind_group
     }
 }
 
-pub struct NodeTexture {
-    pub texture: RGBATexture,
-    pub bind_group: wgpu::BindGroup,
+struct InnerNodeTexture {
+    texture: Arc<RGBATexture>,
+    bind_group: Arc<wgpu::BindGroup>,
 }
 
-impl NodeTexture {
+impl InnerNodeTexture {
     pub fn new(ctx: &WgpuCtx, resolution: &Resolution) -> Self {
         let texture = RGBATexture::new(ctx, resolution);
         let bind_group = texture.new_bind_group(ctx, &ctx.rgba_bind_group_layout);
 
         Self {
-            texture,
-            bind_group,
+            texture: Arc::new(texture),
+            bind_group: Arc::new(bind_group),
         }
     }
 }
-pub struct OutputTexture {
-    pub yuv_textures: YUVTextures,
-    buffers: [wgpu::Buffer; 3],
-    pub resolution: Resolution,
+
+pub struct NodeTexture {
+    inner: Mutex<InnerNodeTexture>,
+    resolution: Resolution,
 }
 
-impl OutputTexture {
-    fn padded(width: usize) -> usize {
-        width + (256 - (width % 256))
-    }
+impl NodeTexture {
     pub fn new(ctx: &WgpuCtx, resolution: &Resolution) -> Self {
-        let device = &ctx.device;
-        let buffers = [
-            device.create_buffer(&wgpu::BufferDescriptor {
-                label: Some("output texture buffer 0"),
-                mapped_at_creation: false,
-                usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
-                size: (Self::padded(resolution.width) * resolution.height) as u64,
-            }),
-            device.create_buffer(&wgpu::BufferDescriptor {
-                label: Some("output texture buffer 1"),
-                mapped_at_creation: false,
-                usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
-                size: (Self::padded(resolution.width / 2) * resolution.height / 2) as u64,
-            }),
-            device.create_buffer(&wgpu::BufferDescriptor {
-                label: Some("output texture buffer 2"),
-                mapped_at_creation: false,
-                usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
-                size: (Self::padded(resolution.width / 2) * resolution.height / 2) as u64,
-            }),
-        ];
         Self {
-            yuv_textures: YUVTextures::new(ctx, resolution),
-            buffers,
+            inner: InnerNodeTexture::new(ctx, resolution).into(),
             resolution: *resolution,
         }
     }
 
-    /// extremely hacky workaround to download texture
-    /// TODO: do it properly, potentially we might want to use DownloadBuffer
-    pub fn download(&self, ctx: &WgpuCtx) -> YuvData {
-        self.transfer_content_to_buffers(&ctx.device, &ctx.queue);
-        let mut result = YuvData {
-            y_plane: Bytes::new(),
-            u_plane: Bytes::new(),
-            v_plane: Bytes::new(),
-        };
-        {
-            let size = self.yuv_textures.0[0].texture.size();
-            let buffer = BytesMut::with_capacity(size.width as usize * size.height as usize);
-
-            self.buffers[0]
-                .slice(..)
-                .map_async(MapMode::Read, |x| x.unwrap());
-            ctx.device.poll(wgpu::MaintainBase::Wait);
-
-            let range = self.buffers[0].slice(..).get_mapped_range();
-            let chunks = range.chunks(Self::padded(size.width as usize));
-            let mut buffer = buffer.writer();
-            for chunk in chunks {
-                buffer.write_all(&chunk[..size.width as usize]).unwrap();
-            }
-            result.y_plane = buffer.into_inner().into();
+    pub fn ensure_size(&self, ctx: &WgpuCtx, resolution: &Resolution) {
+        if *resolution != self.resolution {
+            let new_inner = InnerNodeTexture::new(ctx, resolution);
+            let mut guard = self.inner.lock().unwrap();
+            let _ = mem::replace(guard.deref_mut(), new_inner);
         }
-        {
-            let size = self.yuv_textures.0[1].texture.size();
-            let buffer = BytesMut::with_capacity(size.width as usize * size.height as usize);
-
-            self.buffers[1]
-                .slice(..)
-                .map_async(MapMode::Read, |x| x.unwrap());
-            ctx.device.poll(wgpu::MaintainBase::Wait);
-
-            let range = self.buffers[1].slice(..).get_mapped_range();
-            let chunks = range.chunks(Self::padded(size.width as usize));
-            let mut buffer = buffer.writer();
-            for chunk in chunks {
-                buffer.write_all(&chunk[..size.width as usize]).unwrap();
-            }
-            result.u_plane = buffer.into_inner().into();
-        }
-        {
-            let size = self.yuv_textures.0[2].texture.size();
-            let buffer = BytesMut::with_capacity(size.width as usize * size.height as usize);
-
-            self.buffers[2]
-                .slice(..)
-                .map_async(MapMode::Read, |x| x.unwrap());
-            ctx.device.poll(wgpu::MaintainBase::Wait);
-
-            let range = self.buffers[2].slice(..).get_mapped_range();
-            let chunks = range.chunks(Self::padded(size.width as usize));
-            let mut buffer = buffer.writer();
-            for chunk in chunks {
-                buffer.write_all(&chunk[..size.width as usize]).unwrap();
-            }
-            result.v_plane = buffer.into_inner().into();
-        }
-        for buffer in self.buffers.iter() {
-            buffer.unmap();
-        }
-        result
     }
 
-    fn transfer_content_to_buffers(&self, device: &wgpu::Device, queue: &wgpu::Queue) {
-        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: Some("transfer result yuv texture to buffers encoder"),
+    pub fn rgba_texture(&self) -> Arc<RGBATexture> {
+        let guard = self.inner.lock().unwrap();
+        guard.texture.clone()
+    }
+
+    pub fn bind_group(&self) -> Arc<wgpu::BindGroup> {
+        let guard = self.inner.lock().unwrap();
+        guard.bind_group.clone()
+    }
+}
+
+pub struct OutputTexture {
+    textures: YUVTextures,
+    buffers: [wgpu::Buffer; 3],
+    resolution: Resolution,
+}
+
+impl OutputTexture {
+    pub fn new(ctx: &WgpuCtx, resolution: &Resolution) -> Self {
+        let textures = YUVTextures::new(ctx, resolution);
+        let buffers = textures.new_download_buffers(ctx);
+        Self {
+            textures,
+            buffers,
+            resolution: resolution.to_owned(),
+        }
+    }
+
+    pub fn yuv_textures(&self) -> &YUVTextures {
+        &self.textures
+    }
+
+    pub fn resolution(&self) -> &Resolution {
+        &self.resolution
+    }
+
+    pub fn start_download<'a>(
+        &'a self,
+        ctx: &WgpuCtx,
+    ) -> YUVPendingDownload<
+        'a,
+        impl FnOnce() -> Result<Bytes, BufferAsyncError> + 'a,
+        BufferAsyncError,
+    > {
+        self.textures.copy_to_buffers(ctx, &self.buffers);
+
+        YUVPendingDownload::new(
+            self.download_buffer(self.textures.planes[0].texture.size(), &self.buffers[0]),
+            self.download_buffer(self.textures.planes[1].texture.size(), &self.buffers[1]),
+            self.download_buffer(self.textures.planes[2].texture.size(), &self.buffers[2]),
+        )
+    }
+
+    fn download_buffer<'a>(
+        &'a self,
+        size: wgpu::Extent3d,
+        source: &'a Buffer,
+    ) -> impl FnOnce() -> Result<Bytes, BufferAsyncError> + 'a {
+        let buffer = BytesMut::with_capacity((size.width * size.height) as usize);
+        let (s, r) = bounded(1);
+        source.slice(..).map_async(MapMode::Read, move |result| {
+            if let Err(err) = s.send(result) {
+                error!("channel send error: {err}")
+            }
         });
 
-        for plane in [0, 1, 2] {
-            encoder.copy_texture_to_buffer(
-                wgpu::ImageCopyTexture {
-                    aspect: wgpu::TextureAspect::All,
-                    mip_level: 0,
-                    origin: wgpu::Origin3d::ZERO,
-                    texture: &self.yuv_textures.0[plane].texture,
-                },
-                wgpu::ImageCopyBuffer {
-                    buffer: &self.buffers[plane],
-                    layout: wgpu::ImageDataLayout {
-                        bytes_per_row: Some(Self::padded(
-                            self.yuv_textures.0[plane].texture.size().width as usize,
-                        ) as u32),
-                        rows_per_image: Some(self.yuv_textures.0[plane].texture.size().height),
-                        offset: 0,
-                    },
-                },
-                self.yuv_textures.0[plane].texture.size(),
-            )
+        move || {
+            r.recv().unwrap()?;
+            let mut buffer = buffer.writer();
+            {
+                let range = source.slice(..).get_mapped_range();
+                let chunks = range.chunks(pad_to_256(size.width) as usize);
+                for chunk in chunks {
+                    buffer.write_all(&chunk[..size.width as usize]).unwrap();
+                }
+            };
+            source.unmap();
+            Ok(buffer.into_inner().into())
         }
-
-        queue.submit(Some(encoder.finish()));
     }
 }
---
+++ compositor_render/src/renderer/texture/rgba.rs
@@ -0,0 +1,64 @@
+use compositor_common::scene::Resolution;
+
+use crate::renderer::WgpuCtx;
+
+use super::base::Texture;
+
+pub struct RGBATexture(Texture);
+
+impl RGBATexture {
+    pub fn new(ctx: &WgpuCtx, resolution: &Resolution) -> Self {
+        Self(Texture::new(
+            ctx,
+            None,
+            wgpu::Extent3d {
+                width: resolution.width as u32,
+                height: resolution.height as u32,
+                depth_or_array_layers: 1,
+            },
+            wgpu::TextureFormat::Rgba8Unorm,
+            wgpu::TextureUsages::RENDER_ATTACHMENT
+                | wgpu::TextureUsages::COPY_DST
+                | wgpu::TextureUsages::TEXTURE_BINDING,
+        ))
+    }
+
+    pub fn new_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("single texture bind group layout"),
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                ty: Texture::DEFAULT_BINDING_TYPE,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                count: None,
+            }],
+        })
+    }
+
+    pub(super) fn new_bind_group(
+        &self,
+        ctx: &WgpuCtx,
+        layout: &wgpu::BindGroupLayout,
+    ) -> wgpu::BindGroup {
+        ctx.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("texture bind group"),
+            layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&self.0.view),
+            }],
+        })
+    }
+
+    pub fn upload(&self, ctx: &WgpuCtx, data: &[u8]) {
+        self.0.upload_data(&ctx.queue, data, 4);
+    }
+
+    pub fn size(&self) -> wgpu::Extent3d {
+        self.0.size()
+    }
+
+    pub fn texture(&self) -> &Texture {
+        &self.0
+    }
+}
