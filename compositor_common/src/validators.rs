858e1564a5087df96870b7044469653b16065989
Wojciech Barczynski <barczynski.w@gmail.com>
2023-08-04 12:07:46 +0200

    Add cycles detection

--- compositor_common/src/validators.rs
+++ compositor_common/src/validators.rs
@@ -1,6 +1,9 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
 
-use crate::scene::{NodeId, SceneSpec};
+use crate::scene::{NodeId, SceneSpec, TransformNodeSpec};
 
 #[derive(Debug, thiserror::Error)]
 pub enum SpecValidationError {
@@ -14,6 +17,8 @@ pub enum SpecValidationError {
         missing_node: Arc<str>,
         output: Arc<str>,
     },
+    #[error("Detected cycle in scene graph. Scene should acyclic graph")]
+    CycleDetected,
     #[error("unknown output, output with id {0} is not registered currently")]
     UnknownOutput(Arc<str>),
     #[error("unknown input, input with id {0} is not registered currently")]
@@ -41,11 +46,12 @@ impl SceneSpec {
         self.validate_inputs(registered_inputs)?;
         self.validate_transform_inputs(&defined_node_ids)?;
         self.validate_outputs(registered_outputs, &defined_node_ids)?;
+        self.validate_cycles(registered_inputs)?;
 
         Ok(())
     }
 
-    pub fn validate_transform_inputs(
+    fn validate_transform_inputs(
         &self,
         defined_node_ids: &HashSet<NodeId>,
     ) -> Result<(), SpecValidationError> {
@@ -63,7 +69,7 @@ impl SceneSpec {
         Ok(())
     }
 
-    pub fn validate_inputs(
+    fn validate_inputs(
         &self,
         registered_inputs: &HashSet<NodeId>,
     ) -> Result<(), SpecValidationError> {
@@ -78,7 +84,7 @@ impl SceneSpec {
         Ok(())
     }
 
-    pub fn validate_outputs(
+    fn validate_outputs(
         &self,
         registered_outputs: &HashSet<NodeId>,
         defined_node_ids: &HashSet<NodeId>,
@@ -97,5 +103,136 @@ impl SceneSpec {
         }
 
         Ok(())
+    }
+
+    fn validate_cycles(
+        &self,
+        registered_inputs: &HashSet<NodeId>,
+    ) -> Result<(), SpecValidationError> {
+        enum NodeState {
+            BeingVisited,
+            Visited,
+        }
+
+        let nodes: HashMap<&NodeId, &TransformNodeSpec> = self
+            .transforms
+            .iter()
+            .map(|node| (&node.node_id, node))
+            .collect();
+
+        let mut visited: HashMap<NodeId, NodeState> = HashMap::new();
+
+        fn visit(
+            node: &NodeId,
+            transform_nodes: &HashMap<&NodeId, &TransformNodeSpec>,
+            input_nodes: &HashSet<NodeId>,
+            visited: &mut HashMap<NodeId, NodeState>,
+        ) -> Result<(), SpecValidationError> {
+            if let Some(_input_node) = input_nodes.get(node) {
+                return Ok(());
+            }
+
+            match visited.get(node) {
+                Some(NodeState::BeingVisited) => return Err(SpecValidationError::CycleDetected),
+                Some(NodeState::Visited) => return Ok(()),
+                None => {}
+            }
+
+            visited.insert(node.clone(), NodeState::BeingVisited);
+
+            for child in &transform_nodes.get(&node).unwrap().input_pads {
+                visit(child, transform_nodes, input_nodes, visited)?;
+            }
+
+            visited.insert(node.clone(), NodeState::Visited);
+
+            Ok(())
+        }
+
+        for output in &self.outputs {
+            visit(&output.input_pad, &nodes, registered_inputs, &mut visited)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        collections::{HashMap, HashSet},
+        sync::Arc,
+    };
+
+    use crate::{
+        scene::{
+            InputId, InputSpec, NodeId, OutputId, OutputSpec, Resolution, SceneSpec,
+            TransformNodeSpec, TransformParams,
+        },
+        transformation::TransformationRegistryKey,
+        validators::SpecValidationError,
+    };
+
+    #[test]
+    fn scene_validation_finds_cycle() {
+        let res = Resolution {
+            width: 1920,
+            height: 1080,
+        };
+        let trans_parms = TransformParams::Shader {
+            shader_id: TransformationRegistryKey(Arc::from("shader")),
+            shader_params: HashMap::new(),
+        };
+
+        let input_id = NodeId(Arc::from("input"));
+        let a_id = NodeId(Arc::from("a"));
+        let b_id = NodeId(Arc::from("b"));
+        let c_id = NodeId(Arc::from("c"));
+        let output_id = NodeId(Arc::from("output"));
+
+        let input = InputSpec {
+            input_id: InputId(input_id.clone()),
+            resolution: res,
+        };
+
+        let a = TransformNodeSpec {
+            node_id: a_id.clone(),
+            input_pads: vec![input_id.clone(), c_id.clone()],
+            resolution: res,
+            transform_params: trans_parms.clone(),
+        };
+
+        let b = TransformNodeSpec {
+            node_id: b_id.clone(),
+            input_pads: vec![a_id.clone()],
+            resolution: res,
+            transform_params: trans_parms.clone(),
+        };
+
+        let c = TransformNodeSpec {
+            node_id: c_id.clone(),
+            input_pads: vec![b_id.clone()],
+            resolution: res,
+            transform_params: trans_parms.clone(),
+        };
+
+        let output = OutputSpec {
+            output_id: OutputId(output_id.clone()),
+            input_pad: c_id,
+        };
+
+        let scene_spec = SceneSpec {
+            inputs: vec![input],
+            transforms: vec![a, b, c],
+            outputs: vec![output],
+        };
+
+        let registered_inputs = HashSet::from([input_id]);
+        let registered_outputs = HashSet::from([output_id]);
+
+        assert!(matches!(
+            scene_spec.validate(&registered_inputs, &registered_outputs),
+            Err(SpecValidationError::CycleDetected)
+        ));
     }
 }
