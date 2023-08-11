5c60937cfd93900e7c110986f864cbb18a2336f7
Mikołaj Radkowski <mikolaj.radkowski@protonmail.com>
2023-08-11 10:55:39 +0200

    Bundle main app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use compositor_chromium::cef;
 use log::info;
 
 mod api;
@@ -9,6 +10,16 @@ fn main() {
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
+
+    let target_path = std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_owned();
+    if cef::bundle_app(&target_path).is_err() {
+        panic!("Build process helper first: cargo build --bin process_helper");
+    }
+
     ffmpeg_next::format::network::init();
 
     http::Server::new(8001).start();
