ad21fa405e7e7bec1bd9c99900c66cb83c4c8128
noituri <mikolaj.radkowski@protonmail.com>
2023-09-05 20:51:36 +0200

    Add errors in `V8Object`

--- compositor_chromium/src/v8/object.rs
+++ compositor_chromium/src/v8/object.rs
@@ -1,16 +1,33 @@
-use crate::{cef_string::CefString, validated::Validated};
+use crate::{
+    cef_string::CefString,
+    validated::{Validated, ValidatedError},
+};
 
-use super::value::{V8Value, V8ValueError};
+use super::{value::V8Value, V8ValueError};
 
 pub struct V8Object(pub(super) Validated<chromium_sys::cef_v8value_t>);
 
 impl V8Object {
-    pub fn has(&self, key: &str) -> Result<bool, V8ValueError> {
+    pub fn has(&self, key: &str) -> Result<bool, V8ObjectError> {
         let inner = self.0.get()?;
         let key = CefString::new_raw(key);
         unsafe {
             let has_value = (*inner).has_value_bykey.unwrap();
             Ok(has_value(inner, &key) == 1)
+        }
+    }
+
+    pub fn get(&self, key: &str) -> Result<V8Value, V8ObjectError> {
+        let inner = self.0.get()?;
+        let key = CefString::new_raw(key);
+        unsafe {
+            let get_value = (*inner).get_value_bykey.unwrap();
+            let value = get_value(inner, &key);
+            if value.is_null() {
+                return Err(V8ObjectError::FieldNotFound);
+            }
+
+            Ok(V8Value::from_raw(value))
         }
     }
 
@@ -20,22 +37,29 @@ impl V8Object {
         key: &str,
         value: &V8Value,
         attribute: V8PropertyAttribute,
-    ) -> Result<bool, V8ValueError> {
+    ) -> Result<(), V8ObjectError> {
         let inner = self.0.get()?;
         let key = CefString::new_raw(key);
         unsafe {
             let set_value = (*inner).set_value_bykey.unwrap();
-            Ok(set_value(inner, &key, value.get_raw()?, attribute as u32) == 1)
+            if set_value(inner, &key, value.get_raw()?, attribute as u32) != 1 {
+                return Err(V8ObjectError::SetFailed);
+            }
+            Ok(())
         }
     }
 
     /// Returns `true` if value was deleted successfully
-    pub fn delete(&mut self, key: &str) -> Result<bool, V8ValueError> {
+    pub fn delete(&mut self, key: &str) -> Result<(), V8ObjectError> {
         let inner = self.0.get()?;
         let key = CefString::new_raw(key);
         unsafe {
             let delete_value = (*inner).delete_value_bykey.unwrap();
-            Ok(delete_value(inner, &key) == 1)
+            if delete_value(inner, &key) != 1 {
+                return Err(V8ObjectError::DeleteFailed);
+            }
+
+            Ok(())
         }
     }
 }
@@ -46,4 +70,22 @@ pub enum V8PropertyAttribute {
     ReadOnly = chromium_sys::cef_v8_propertyattribute_t_V8_PROPERTY_ATTRIBUTE_READONLY,
     DoNotEnum = chromium_sys::cef_v8_propertyattribute_t_V8_PROPERTY_ATTRIBUTE_DONTENUM,
     DoNotDelete = chromium_sys::cef_v8_propertyattribute_t_V8_PROPERTY_ATTRIBUTE_DONTDELETE,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum V8ObjectError {
+    #[error("V8Object is no longer valid")]
+    ObjectNotValid(#[from] ValidatedError),
+
+    #[error(transparent)]
+    V8ValueError(#[from] V8ValueError),
+
+    #[error("V8Object field not found")]
+    FieldNotFound,
+
+    #[error("Failed to set V8Object field")]
+    SetFailed,
+
+    #[error("Failed to delete V8Object field")]
+    DeleteFailed,
 }
