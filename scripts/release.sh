85f492a487e6bb9c19d1e1d73f784f5c73eb7f47
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-10-02 14:56:50 +0200

    remove `.app` from artifact name in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,9 +23,9 @@ mkdir -p "$ROOT_DIR/release_tmp"
 cd "$ROOT_DIR/release_tmp"
 
 gh run download "$WORKFLOW_RUN_ID" -n video_compositor_linux_x86_64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n video_compositor_darwin_x86_64.app.tar.gz
+gh run download "$WORKFLOW_RUN_ID" -n video_compositor_darwin_x86_64.tar.gz
 gh release create "$RELEASE_TAG"
 gh release upload "$RELEASE_TAG" video_compositor_linux_x86_64.tar.gz
-gh release upload "$RELEASE_TAG" video_compositor_darwin_x86_64.app.tar.gz
+gh release upload "$RELEASE_TAG" video_compositor_darwin_x86_64.tar.gz
 
 rm -rf "$ROOT_DIR/release_tmp"
