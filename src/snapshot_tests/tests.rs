c8517e32665cb3fb916f2f824a072d05c963390b
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-10-23 14:15:07 +0200

    Add support for schemas in vscode

--- src/snapshot_tests/tests.rs
+++ src/snapshot_tests/tests.rs
@@ -4,8 +4,8 @@ pub fn snapshot_tests() -> Vec<TestCase> {
     vec![TestCase {
         name: "basic/test",
         inputs: vec![TestInput::new(1)],
-        renderers: vec![include_str!("../../snapshot_tests/image_renderer.json")],
-        scene_json: include_str!("../../snapshot_tests/basic_scene.json"),
+        renderers: vec![include_str!("../../snapshot_tests/image.renderer.json")],
+        scene_json: include_str!("../../snapshot_tests/basic.scene.json"),
         ..Default::default()
     }]
 }
