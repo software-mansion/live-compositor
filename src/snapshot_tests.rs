8947554a2104e2a4d089e87cfa1ab23326f2c3b1
Mikołaj Radkowski <mikolaj.radkowski@protonmail.com>
2023-11-02 11:01:26 +0100

    Snapshot tests (part 3) (#233)

--- src/snapshot_tests.rs
+++ src/snapshot_tests.rs
@@ -1,7 +1,11 @@
-use std::{collections::HashSet, env};
+use std::{collections::HashSet, env, fs, path::PathBuf};
+
+use anyhow::Error;
+
+use crate::snapshot_tests::test_case::TestCase;
 
 use self::{
-    test_case::Snapshot,
+    test_case::TestCaseError,
     tests::snapshot_tests,
     utils::{find_unused_snapshots, snapshots_path},
 };
@@ -12,22 +16,66 @@ mod utils;
 
 #[test]
 fn test_snapshots() {
-    if env::var("CI").is_ok() {
-        return;
+    let failed_snapshot_path = failed_snapshot_path();
+    if failed_snapshot_path.exists() {
+        fs::remove_dir_all(failed_snapshot_path).unwrap();
     }
 
-    let mut produced_snapshots = HashSet::new();
-    for snapshot_test in snapshot_tests() {
-        eprintln!("Test \"{}\"", snapshot_test.name);
-        let snapshots = snapshot_test.generate_snapshots().unwrap();
-        match snapshot_test.test_snapshots(&snapshots) {
-            Ok(_) => produced_snapshots.extend(snapshots.iter().map(Snapshot::save_path)),
-            Err(err) => panic!("{err}"),
+    let test_cases = snapshot_tests();
+    for test_case in test_cases.iter() {
+        eprintln!("Test \"{}\"", test_case.name);
+        let snapshots = test_case.generate_snapshots().unwrap();
+        if let Err(err) = test_case.test_snapshots(&snapshots) {
+            handle_error(err);
         }
     }
 
-    let unused_snapshots = find_unused_snapshots(&produced_snapshots, snapshots_path());
+    // Check for unused snapshots
+    let snapshot_paths = test_cases
+        .iter()
+        .flat_map(TestCase::snapshot_paths)
+        .collect::<HashSet<_>>();
+    let unused_snapshots = find_unused_snapshots(&snapshot_paths, snapshots_path());
     if !unused_snapshots.is_empty() {
         panic!("Some snapshots were not used: {unused_snapshots:#?}")
     }
+}
+
+fn handle_error(err: Error) {
+    let test_case_err = err.downcast_ref::<TestCaseError>().unwrap();
+    let TestCaseError::Mismatch {
+        snapshot_from_disk,
+        produced_snapshot,
+    } = test_case_err
+    else {
+        panic!("{err}");
+    };
+
+    let failed_snapshot_path = failed_snapshot_path();
+    if !failed_snapshot_path.exists() {
+        fs::create_dir_all(&failed_snapshot_path).unwrap();
+    }
+    let snapshot_save_path = produced_snapshot.save_path();
+    let snapshot_name = snapshot_save_path.file_name().unwrap().to_string_lossy();
+
+    let width = produced_snapshot.resolution.width - (produced_snapshot.resolution.width % 2);
+    let height = produced_snapshot.resolution.height - (produced_snapshot.resolution.height % 2);
+    image::save_buffer(
+        failed_snapshot_path.join(format!("mismatched_{snapshot_name}")),
+        &produced_snapshot.data,
+        width as u32,
+        height as u32,
+        image::ColorType::Rgba8,
+    )
+    .unwrap();
+
+    snapshot_from_disk
+        .save(failed_snapshot_path.join(format!("original_{snapshot_name}")))
+        .unwrap();
+
+    panic!("{err}");
+}
+
+fn failed_snapshot_path() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("failed_snapshot_tests")
 }
