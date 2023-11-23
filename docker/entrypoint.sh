49ad4f019ca7e7a7912d4f73a290323e241cfa5a
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-11-23 16:41:32 +0100

    Configure multi-staged docker build (#264)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,11 +3,6 @@
 set -eo pipefail
 set -x
 
-source ~/.cargo/env
-
-sudo dbus-daemon --system
-sudo Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
-
 sleep 2
 
 exec "$MEMBRANE_VIDEO_COMPOSITOR_MAIN_EXECUTABLE_PATH"
