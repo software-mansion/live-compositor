be7451dd3f667dbe09d9884e97bc7601ad76396d
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-11-24 16:08:27 +0100

    deploy

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://compositor.live',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/video_compositor/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
