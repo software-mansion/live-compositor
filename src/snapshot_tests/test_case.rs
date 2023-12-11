cf32fd2637810723c867417c55fb3d2770c6f7bf
Mikołaj Radkowski <mikolaj.radkowski@protonmail.com>
2023-12-11 16:00:42 +0100

    [snapshot tests] Add test inputs with patterns (#298)

--- src/snapshot_tests/test_case.rs
+++ src/snapshot_tests/test_case.rs
@@ -7,6 +7,7 @@ use compositor_common::{
     frame::YuvData,
     renderer_spec::RendererSpec,
     scene::{InputId, OutputId, Resolution},
+    util::colors::RGBColor,
     Frame,
 };
 use compositor_pipeline::pipeline;
@@ -38,7 +39,7 @@ impl Default for TestCase {
             timestamps: vec![Duration::from_secs(0)],
             outputs: Outputs::Scene(vec![]),
             only: false,
-            allowed_error: 130.0,
+            allowed_error: 20.0,
         }
     }
 }
@@ -224,41 +225,41 @@ pub struct TestInput {
 }
 
 impl TestInput {
-    const COLOR_VARIANTS: [(u8, u8, u8); 17] = [
+    const COLOR_VARIANTS: [RGBColor; 17] = [
         // RED, input_0
-        (255, 0, 0),
-        // BLUE, input_1
-        (0, 0, 255),
-        // GREEN, input_2
-        (0, 255, 0),
-        // YELLOW, input_3
-        (255, 255, 0),
-        // MAGENTA, input_4
-        (255, 0, 255),
+        RGBColor(255, 0, 0),
+        // GREEN, input_1
+        RGBColor(0, 255, 0),
+        // YELLOW, input_2
+        RGBColor(255, 255, 0),
+        // MAGENTA, input_3
+        RGBColor(255, 0, 255),
+        // BLUE, input_4
+        RGBColor(0, 0, 255),
         // CYAN, input_5
-        (0, 255, 255),
+        RGBColor(0, 255, 255),
         // ORANGE, input_6
-        (255, 165, 0),
+        RGBColor(255, 165, 0),
         // WHITE, input_7
-        (255, 255, 255),
+        RGBColor(255, 255, 255),
         // GRAY, input_8
-        (128, 128, 128),
+        RGBColor(128, 128, 128),
         // LIGHT_RED, input_9
-        (255, 128, 128),
+        RGBColor(255, 128, 128),
         // LIGHT_BLUE, input_10
-        (128, 128, 255),
+        RGBColor(128, 128, 255),
         // LIGHT_GREEN, input_11
-        (128, 255, 128),
+        RGBColor(128, 255, 128),
         // PINK, input_12
-        (255, 192, 203),
+        RGBColor(255, 192, 203),
         // PURPLE, input_13
-        (128, 0, 128),
+        RGBColor(128, 0, 128),
         // BROWN, input_14
-        (165, 42, 42),
+        RGBColor(165, 42, 42),
         // YELLOW_GREEN, input_15
-        (154, 205, 50),
+        RGBColor(154, 205, 50),
         // LIGHT_YELLOW, input_16
-        (255, 255, 224),
+        RGBColor(255, 255, 224),
     ];
 
     pub fn new(index: usize) -> Self {
@@ -272,22 +273,50 @@ impl TestInput {
     }
 
     pub fn new_with_resolution(index: usize, resolution: Resolution) -> Self {
-        if index >= Self::COLOR_VARIANTS.len() {
-            panic!("Reached input amount limit: {}", Self::COLOR_VARIANTS.len())
+        let color = Self::COLOR_VARIANTS[index].to_yuv();
+        let mut y_plane = vec![0; resolution.width * resolution.height];
+        let mut u_plane = vec![0; (resolution.width * resolution.height) / 4];
+        let mut v_plane = vec![0; (resolution.width * resolution.height) / 4];
+
+        let yuv_color = |x: usize, y: usize| {
+            const BORDER_SIZE: usize = 18;
+            const GRID_SIZE: usize = 72;
+
+            let is_border_in_x =
+                x <= BORDER_SIZE || (x <= resolution.width && x >= resolution.width - BORDER_SIZE);
+            let is_border_in_y: bool = y <= BORDER_SIZE
+                || (y <= resolution.height && y >= resolution.height - BORDER_SIZE);
+            let is_on_grid = (x / GRID_SIZE + y / GRID_SIZE) % 2 == 0;
+
+            let mut y = color.0;
+            if is_border_in_x || is_border_in_y || is_on_grid {
+                y -= 0.2;
+            }
+
+            (y.clamp(0.0, 1.0), color.1, color.2)
+        };
+
+        for x_coord in 0..resolution.width {
+            for y_coord in 0..resolution.height {
+                let (y, u, v) = yuv_color(x_coord, y_coord);
+                if x_coord % 2 == 0 && y_coord % 2 == 0 {
+                    let (_, u2, v2) = yuv_color(x_coord + 1, y_coord);
+                    let (_, u3, v3) = yuv_color(x_coord, y_coord + 1);
+                    let (_, u4, v4) = yuv_color(x_coord + 1, y_coord + 1);
+
+                    let coord = (y_coord / 2) * (resolution.width / 2) + (x_coord / 2);
+                    u_plane[coord] = ((u + u2 + u3 + u4) * 64.0) as u8;
+                    v_plane[coord] = ((v + v2 + v3 + v4) * 64.0) as u8;
+                }
+
+                y_plane[y_coord * resolution.width + x_coord] = (y * 255.0) as u8;
+            }
         }
 
-        let color = Self::COLOR_VARIANTS[index];
-        let r = color.0 as f32;
-        let g = color.1 as f32;
-        let b = color.2 as f32;
-
-        let y = (r * 0.299 + g * 0.587 + b * 0.144).clamp(0.0, 255.0);
-        let u = (r * -0.168736 + g * -0.331264 + b * 0.5 + 128.0).clamp(0.0, 255.0);
-        let v = (r * 0.5 + g * -0.418688 + b * -0.081312 + 128.0).clamp(0.0, 255.0);
         let data = YuvData {
-            y_plane: vec![y as u8; resolution.width * resolution.height].into(),
-            u_plane: vec![u as u8; (resolution.width * resolution.height) / 4].into(),
-            v_plane: vec![v as u8; (resolution.width * resolution.height) / 4].into(),
+            y_plane: y_plane.into(),
+            u_plane: u_plane.into(),
+            v_plane: v_plane.into(),
         };
 
         Self {
