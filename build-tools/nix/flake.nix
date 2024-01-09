b74d55a4579385daf10f4ec6c5065469a67b1550
Wojciech Kozyra <wkozyra95@gmail.com>
2024-01-09 17:29:55 +0100

    Fix nix config for some environments

--- build-tools/nix/flake.nix
+++ build-tools/nix/flake.nix
@@ -1,59 +1,90 @@
 {
   description = "Dev shell for LiveCompositor";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, lib, ... }:
+        let
+          maybeDarwinPkgs = with pkgs; lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Metal
+            darwin.apple_sdk.frameworks.Foundation
+            darwin.apple_sdk.frameworks.QuartzCore
+            darwin.libobjc
+
+            libiconv
+          ];
+          maybeLinuxPkgs = with pkgs; lib.optionals stdenv.isLinux [
+            vulkan-tools
+            vulkan-headers
+            vulkan-loader
+            vulkan-validation-layers
+            wayland
+            mesa.drivers
+          ];
+          # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/libcef/default.nix#L33
+          libcefDependencies = with pkgs;  [
+            glib
+            nss
+            nspr
+            atk
+            at-spi2-atk
+            expat
+            xorg.libxcb
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            mesa
+            gtk3
+            pango
+            cairo
+            dbus
+            at-spi2-core
+            cups
+            xorg.libxshmfence
+          ] ++ (
+            pkgs.lib.optionals pkgs.stdenv.isLinux [
+              libdrm
+              alsa-lib
+            ]
+          );
+          compositorBuildDependencies = with pkgs; [
+            cargo
+            rustc
+            libGL
+            libopus
+
+            pkg-config
+            openssl
+            ffmpeg_6-full
+
+            llvmPackages_16.clang
+          ] ++ maybeDarwinPkgs ++ maybeLinuxPkgs;
+          devDependencies = with pkgs; [
+            nodejs_18
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+        in
         {
           devShells = {
-            name = "live_compositor";
             default = pkgs.mkShell {
-              packages = with pkgs; [
-                cargo
-                rustc
-                nodejs_18
+              packages = compositorBuildDependencies ++ devDependencies;
 
-                pkg-config
-                openssl
-                ffmpeg_6-full
-                libcef
-                llvmPackages.bintools
-                llvmPackages.clang
-              ];
-              env.LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-              env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
-                  libGL
-                  
-                  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/libcef/default.nix#L33
-                  glib
-                  nss
-                  nspr
-                  atk
-                  at-spi2-atk
-                  libdrm
-                  expat
-                  xorg.libxcb
-                  libxkbcommon
-                  xorg.libX11
-                  xorg.libXcomposite
-                  xorg.libXdamage
-                  xorg.libXext
-                  xorg.libXfixes
-                  xorg.libXrandr
-                  mesa
-                  gtk3
-                  pango
-                  cairo
-                  alsa-lib
-                  dbus
-                  at-spi2-core
-                  cups
-                  xorg.libxshmfence
-              ]);
-              inputsFrom = [ ];
+              # Fixes "ffplay" used in examples on Linux (not needed on NixOS)
+              env.LIBGL_DRIVERS_PATH = "${pkgs.mesa.drivers}/lib/dri";
+
+              env.LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+              env.LD_LIBRARY_PATH = lib.makeLibraryPath (maybeDarwinPkgs ++ maybeLinuxPkgs ++ libcefDependencies ++ [ pkgs.libGL ]);
             };
           };
         };
