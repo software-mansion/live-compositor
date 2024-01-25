58d5a7e21cfee7bc221fc044748353e6c79512fe
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-01-25 14:39:28 +0100

    Add `package` to nix config + fix shell for macOS (#355)

--- build-tools/nix/flake.nix
+++ build-tools/nix/flake.nix
@@ -1,60 +1,83 @@
 {
   description = "Dev shell for LiveCompositor";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, lib, ... }:
+        let
+          packageWithoutChromium = (pkgs.callPackage ./package.nix { });
+          ffmpeg =
+            (if pkgs.stdenv.isDarwin then
+              (pkgs.ffmpeg_6-full.override {
+                x264 = pkgs.x264.overrideAttrs (old: {
+                  postPatch = old.postPatch + ''
+                    substituteInPlace Makefile --replace '$(if $(STRIP), $(STRIP) -x $@)' '$(if $(STRIP), $(STRIP) -S $@)'
+                  '';
+                });
+              })
+            else
+              pkgs.ffmpeg_6-full
+            );
+          # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/libcef/default.nix#L33
+          libcefDependencies = with pkgs;  [
+            glib
+            nss
+            nspr
+            atk
+            at-spi2-atk
+            expat
+            xorg.libxcb
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            mesa
+            gtk3
+            pango
+            cairo
+            dbus
+            at-spi2-core
+            cups
+            xorg.libxshmfence
+          ] ++ (
+            pkgs.lib.optionals pkgs.stdenv.isLinux [
+              libdrm
+              alsa-lib
+            ]
+          );
+          devDependencies = with pkgs; [
+            ffmpeg # to add ffplay
+            nodejs_18
+            rustfmt
+            clippy
+            rust-analyzer
+            pkgs.mesa.drivers
+          ];
+        in
         {
           devShells = {
-            name = "live_compositor";
             default = pkgs.mkShell {
-              packages = with pkgs; [
-                cargo
-                rustc
-                nodejs_18
+              packages = devDependencies;
 
-                pkg-config
-                openssl
-                ffmpeg_6-full
-                libcef
-                llvmPackages.bintools
-                llvmPackages.clang
-              ];
-              env.LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-              env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
-                  libGL
-                  
-                  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/libcef/default.nix#L33
-                  glib
-                  nss
-                  nspr
-                  atk
-                  at-spi2-atk
-                  libdrm
-                  expat
-                  xorg.libxcb
-                  libxkbcommon
-                  xorg.libX11
-                  xorg.libXcomposite
-                  xorg.libXdamage
-                  xorg.libXext
-                  xorg.libXfixes
-                  xorg.libXrandr
-                  mesa
-                  gtk3
-                  pango
-                  cairo
-                  alsa-lib
-                  dbus
-                  at-spi2-core
-                  cups
-                  xorg.libxshmfence
-              ]);
-              inputsFrom = [ ];
+              # Fixes "ffplay" used in examples on Linux (not needed on NixOS)
+              env.LIBGL_DRIVERS_PATH = "${pkgs.mesa.drivers}/lib/dri";
+
+              env.LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+              env.LD_LIBRARY_PATH = lib.makeLibraryPath (libcefDependencies ++ [ pkgs.mesa.drivers pkgs.libGL ]);
+
+              inputsFrom = [ packageWithoutChromium ];
             };
+          };
+          packages = {
+            default = packageWithoutChromium;
           };
         };
     };
