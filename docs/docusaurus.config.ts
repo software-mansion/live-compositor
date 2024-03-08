e85dee818b54e583f31d5729a5404c8047445418
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-03-08 12:29:01 +0100

    Fix issue with relative paths when trailing `/` is present

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   onBrokenAnchors: 'throw',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
