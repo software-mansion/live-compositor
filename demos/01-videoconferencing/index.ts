912c68fe714c267b46ba077b10bd30009ce7d562
Wojciech Barczynski <barczynski.w@gmail.com>
2024-05-09 15:54:45 +0200

    forEach -> for

--- demos/01-videoconferencing/index.ts
+++ demos/01-videoconferencing/index.ts
@@ -80,14 +80,15 @@ async function exampleAsync() {
     2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5,
     4, 3, 2, 1,
   ];
-  inputs.forEach(async (input, index) => {
+
+  for (let i = 0; i < inputs.length; i++) {
     await updateOutputAsync("output_1", {
       video: {
-        root: sceneWithInputs(input),
+        root: sceneWithInputs(inputs[i]),
       },
-      schedule_time_ms: 2000 * (index + 1),
+      schedule_time_ms: 2000 * (i + 1),
     });
-  });
+  }
 }
 
 function sceneWithInputs(n: number): Component {
