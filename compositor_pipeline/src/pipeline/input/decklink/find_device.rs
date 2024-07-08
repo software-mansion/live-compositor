a5e0d3ae76699fdab1afc38b23db3f75d3edfec9
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-07-08 14:13:11 +0200

    Select DeckLink by persisten ID + add override for pixel format

--- compositor_pipeline/src/pipeline/input/decklink/find_device.rs
+++ compositor_pipeline/src/pipeline/input/decklink/find_device.rs
@@ -2,17 +2,34 @@ use decklink::{
     get_decklinks, FlagAttributeId, IntegerAttributeId, StringAttributeId, VideoIOSupport,
 };
 
-use super::{DeckLinkError, DeckLinkOptions};
+use super::{DeckLinkError, DeckLinkInfo, DeckLinkOptions};
 
 pub(super) fn find_decklink(opts: &DeckLinkOptions) -> Result<decklink::DeckLink, DeckLinkError> {
     let decklinks = get_decklinks()?;
+
+    let decklinks_info = decklinks
+        .iter()
+        .map(|decklink| {
+            let attr = decklink.profile_attributes()?;
+            Ok(DeckLinkInfo {
+                display_name: attr.get_string(StringAttributeId::DisplayName)?,
+                persistent_id: attr
+                    .get_integer(IntegerAttributeId::PersistentID)?
+                    .map(|value| format!("{value:X}")),
+                subdevice_index: attr
+                    .get_integer(IntegerAttributeId::SubDeviceIndex)?
+                    .map(|i| i as u32),
+            })
+        })
+        .collect::<Result<_, DeckLinkError>>()?;
 
     for mut decklink in decklinks.into_iter() {
         if is_selected_decklink(opts, &mut decklink)? {
             return Ok(decklink);
         }
     }
-    Err(DeckLinkError::NoMatchingDeckLink)
+
+    Err(DeckLinkError::NoMatchingDeckLink(decklinks_info))
 }
 
 fn is_selected_decklink(
@@ -29,6 +46,12 @@ fn is_selected_decklink(
 
     if let Some(display_name) = &opts.display_name {
         if attr.get_string(StringAttributeId::DisplayName)?.as_ref() != Some(display_name) {
+            return Ok(false);
+        }
+    }
+
+    if let Some(persistent_id) = opts.persistent_id {
+        if attr.get_integer(IntegerAttributeId::PersistentID)? != Some(persistent_id as i64) {
             return Ok(false);
         }
     }
--- src/types/from_register_input.rs
+++ src/types/from_register_input.rs
@@ -171,11 +171,28 @@ impl TryFrom<DeckLink> for pipeline::RegisterInputOptions {
 
     #[cfg(feature = "decklink")]
     fn try_from(value: DeckLink) -> Result<Self, Self::Error> {
+        use compositor_pipeline::pipeline::input::decklink;
+
+        const ID_PARSE_ERROR_MESSAGE: &str =
+            "\"persistent_id\" has to be a valid 32-bit hexadecimal number";
+
+        let persistent_id = match value.persistent_id {
+            Some(persistent_id) => {
+                let Ok(persistent_id) = u32::from_str_radix(&persistent_id, 16) else {
+                    return Err(TypeError::new(ID_PARSE_ERROR_MESSAGE));
+                };
+                Some(persistent_id)
+            }
+            None => None,
+        };
+
         Ok(pipeline::RegisterInputOptions {
             input_options: input::InputOptions::DeckLink(input::decklink::DeckLinkOptions {
                 subdevice_index: value.subdevice_index,
                 display_name: value.display_name,
+                persistent_id,
                 enable_audio: value.enable_audio.unwrap_or(true),
+                pixel_format: Some(decklink::PixelFormat::Format8BitYUV),
             }),
             queue_options: queue::QueueInputOptions {
                 required: value.required.unwrap_or(false),
