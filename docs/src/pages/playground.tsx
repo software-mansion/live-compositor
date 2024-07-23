c9e7e816817c493313a4148ecd2f89a4f4029d22
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2024-07-23 17:01:21 +0200

    skeleton of frontend

--- docs/src/pages/playground.tsx
+++ docs/src/pages/playground.tsx
@@ -2,31 +2,106 @@ import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-import styles from './index.module.css';
+import styles from './playground.module.css';
+import { useState } from 'react';
+
+const CodeInputArea = ({ value, onChange }) => (
+  <textarea
+    className={clsx(styles.codeArea)}
+    name="inputArea"
+    placeholder="Enter your code to try it out"
+    onChange={onChange}
+    style={{ height: value, width: '100%' }}
+  />
+);
+
+const ImageDisplay = ({ imageUrl }) => (
+  <div className={clsx(styles.imageDisplay)}>
+    {imageUrl ? (
+      <img
+        src={imageUrl}
+        alt="Generated"
+        style={{
+          objectFit: 'contain',
+          height: '100%',
+          width: '100%',
+        }}
+      />
+    ) : (
+      <div></div>
+    )}
+  </div>
+);
+
+const Options = ({ onSubmit }) => (
+  <div className={clsx('row', styles.options)}>
+    <div className="col">
+      <select>
+        <option value="someOption">Some option</option>
+        <option value="otherOption">Other option</option>
+      </select>
+    </div>
+    <div className="col">
+      <select>
+        <option value="someOption">Some option</option>
+        <option value="otherOption">Other option</option>
+      </select>
+    </div>
+    <div className="col">
+      <select>
+        <option value="someOption">Some option</option>
+        <option value="otherOption">Other option</option>
+      </select>
+    </div>
+    <div className="col">
+      <button className="button button--outline button--primary" onClick={onSubmit}>
+        Submit
+      </button>
+    </div>
+  </div>
+);
 
 function Homepage() {
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleInputChange = event => {
+    setTextAreaValue(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const backendUrl = 'http://localhost:8081/render_image';
+
+    try {
+      const response = await fetch(backendUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: textAreaValue,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const blob = await response.blob();
+      const imageObjectURL = URL.createObjectURL(blob);
+      setImageUrl(imageObjectURL);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className={clsx('container', styles.firstSection)}>
+    <div className={clsx(styles.page)}>
       <div className="row">
-        <div>
-          <textarea name="post" rows={40} cols={100} />
+        <div className={clsx('col col-6', styles.codeArea)}>
+          <CodeInputArea value={textAreaValue} onChange={handleInputChange} />
         </div>
-        <div className={styles.heroVideo}>
-          <div
-            style={{
-              borderRadius: '15px',
-              overflow: 'hidden',
-              boxShadow: '0px 0px 20px 1px #4332a6',
-            }}>
-            <video
-              loop
-              autoPlay
-              muted
-              src="/video/showcase.mp4"
-              poster="/img/showcase_poster.jpg"
-              style={{ width: '100%', display: 'block' }}
-            />
-          </div>
+        <div className="col col-6">
+          <ImageDisplay imageUrl={imageUrl} />
+          <Options onSubmit={handleSubmit} />
         </div>
       </div>
     </div>
@@ -36,9 +111,7 @@ function Homepage() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={siteConfig.title}
-      description="Tool for real-time video processing / transforming / composing">
+    <Layout title={siteConfig.title}>
       <Homepage />
     </Layout>
   );
