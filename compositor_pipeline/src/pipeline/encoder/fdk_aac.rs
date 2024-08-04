c0aeb72bd910b8ab62bbc19e6bc3bead120db673
Wojciech Barczynski <barczynski.w@gmail.com>
2024-08-04 12:18:07 +0200

    Add aac encoder

--- compositor_pipeline/src/pipeline/encoder/fdk_aac.rs
+++ compositor_pipeline/src/pipeline/encoder/fdk_aac.rs
@@ -1,19 +1,19 @@
 use std::{
-    collections::VecDeque,
-    mem::{self, MaybeUninit},
+    mem::{self},
     os::raw::{c_int, c_void},
     ptr,
+    time::Duration,
 };
 
 use bytes::BytesMut;
 use crossbeam_channel::{bounded, Receiver, Sender};
 use fdk_aac_sys as fdk;
-use tracing::{span, Level};
+use tracing::{error, info, span, Level};
 
 use crate::{
     audio_mixer::{AudioChannels, AudioSamples, OutputSamples},
     error::OutputInitError,
-    pipeline::{EncodedChunk, EncoderOutputEvent},
+    pipeline::{AudioCodec, EncodedChunk, EncodedChunkKind, EncoderOutputEvent},
     queue::PipelineEvent,
 };
 
@@ -23,6 +23,9 @@ pub enum AacEncoderError {
     FdkEncoderError(fdk::AACENC_ERROR),
 }
 
+/// FDK-AAC encoder.
+/// Implementation is based on the fdk-aac Encoder documentation:
+/// https://github.com/mstorsjo/fdk-aac/blob/master/documentation/aacEncoder.pdf
 pub struct AacEncoder {
     samples_batch_sender: Sender<PipelineEvent<OutputSamples>>,
 }
@@ -43,7 +46,7 @@ impl AacEncoder {
         std::thread::Builder::new()
             .name("Aac encoder thread".to_string())
             .spawn(move || {
-                let _span = span!(Level::INFO, "Aac encoder thread",).entered();
+                let _span = span!(Level::INFO, "Aac encoder thread").entered();
                 run_encoder_thread(
                     init_result_sender,
                     options,
@@ -66,78 +69,92 @@ impl AacEncoder {
     }
 }
 
+/// Section 2.4.1 of the fdk-aac Encoder documentation:
+/// "The output buffer size should be 6144 bits per channel excluding the LFE channel."
+const FDK_AAC_OUTPUT_BUFFER_CHANNEL_SIZE: usize = 6144;
+
 struct AacEncoderInner {
     encoder: *mut fdk::AACENCODER,
-    input_buffer: VecDeque<i32>,
+    input_buffer: Vec<i32>,
     output_buffer: Vec<u8>,
+    sample_rate: u32,
+    start_pts: Option<Duration>,
+    sent_samples: u128,
 }
 
 impl AacEncoderInner {
     fn new(options: AacEncoderOptions, sample_rate: u32) -> Result<Self, AacEncoderError> {
+        // Section 2.3 of the fdk-aac Encoder documentation - encoder initialization.
         let mut encoder = ptr::null_mut();
-        let info;
-
-        let channels = match options.channels {
-            AudioChannels::Mono => 1,
-            AudioChannels::Stereo => 2,
+        // For mono and stereo audio, those values are the same, but it's not the case for other channel modes.
+        // Leaving as it is, to avoid potential issues when other channel options will be added.
+        let (channels, channel_mode) = match options.channels {
+            AudioChannels::Mono => (1, fdk::CHANNEL_MODE_MODE_1 as u32),
+            AudioChannels::Stereo => (2, fdk::CHANNEL_MODE_MODE_2 as u32),
         };
 
         unsafe {
-            check_fdk(fdk::aacEncOpen(&mut encoder as *mut _, 0, 2))?;
-            check_fdk(fdk::aacEncoder_SetParam(
+            check(fdk::aacEncOpen(&mut encoder as *mut _, 0, channels))?;
+
+            check(fdk::aacEncoder_SetParam(
                 encoder,
                 fdk::AACENC_PARAM_AACENC_BITRATEMODE,
                 5,
             ))?;
-            check_fdk(fdk::aacEncoder_SetParam(
+            check(fdk::aacEncoder_SetParam(
                 encoder,
                 fdk::AACENC_PARAM_AACENC_SAMPLERATE,
                 sample_rate,
             ))?;
-            check_fdk(fdk::aacEncoder_SetParam(
+            check(fdk::aacEncoder_SetParam(
                 encoder,
                 fdk::AACENC_PARAM_AACENC_TRANSMUX,
                 2,
             ))?;
-            check_fdk(fdk::aacEncoder_SetParam(
+            check(fdk::aacEncoder_SetParam(
                 encoder,
                 fdk::AACENC_PARAM_AACENC_SBR_MODE,
                 0,
             ))?;
-            check_fdk(fdk::aacEncoder_SetParam(
+            check(fdk::aacEncoder_SetParam(
                 encoder,
                 fdk::AACENC_PARAM_AACENC_CHANNELMODE,
-                channels,
+                channel_mode,
             ))?;
-            check_fdk(fdk::aacEncoder_SetParam(
+            check(fdk::aacEncoder_SetParam(
                 encoder,
                 fdk::AACENC_PARAM_AACENC_AFTERBURNER,
                 1,
             ))?;
 
-            check_fdk(fdk::aacEncEncode(
+            // Section 2.2.3 of the fdk-aac Encoder documentation:
+            // "Call aacEncEncode() with NULL parameters to initialize encoder instance with present parameter set."
+            check(fdk::aacEncEncode(
                 encoder,
                 ptr::null(),
                 ptr::null(),
                 ptr::null(),
                 ptr::null_mut(),
             ))?;
-
-            let mut maybe_info = MaybeUninit::uninit();
-            check_fdk(fdk::aacEncInfo(encoder, maybe_info.as_mut_ptr()))?;
-            info = maybe_info.assume_init();
         }
 
-        let output_buffer_size = info.maxOutBufBytes as usize;
+        let output_buffer_size = FDK_AAC_OUTPUT_BUFFER_CHANNEL_SIZE * channels as usize;
 
         Ok(Self {
             encoder,
-            input_buffer: VecDeque::new(),
+            input_buffer: Vec::new(),
             output_buffer: vec![0; output_buffer_size],
+            sample_rate,
+            start_pts: None,
+            sent_samples: 0,
         })
     }
 
     fn enqueue_input(&mut self, samples: OutputSamples) {
+        if self.start_pts.is_none() {
+            self.start_pts = Some(samples.start_pts);
+        };
+
         match samples.samples {
             AudioSamples::Mono(mono_samples) => {
                 self.input_buffer
@@ -145,24 +162,46 @@ impl AacEncoderInner {
             }
             AudioSamples::Stereo(stereo_samples) => {
                 for (l, r) in stereo_samples {
-                    self.input_buffer.push_back(l as i32);
-                    self.input_buffer.push_back(r as i32);
+                    self.input_buffer.push(l as i32);
+                    self.input_buffer.push(r as i32);
                 }
             }
         }
     }
 
-    fn encode(&mut self) -> Result<EncodedChunk, AacEncoderError> {
-        let output = BytesMut::new();
+    fn encode(
+        &mut self,
+        output_samples: OutputSamples,
+    ) -> Result<Option<EncodedChunk>, AacEncoderError> {
+        self.enqueue_input(output_samples);
+        self.call_fdk_encode(false)
+    }
 
-        while !self.input_buffer.is_empty() {
-            // For safety, buffer descriptions are created right before calling `aacEncEncode``.
+    fn flush(&mut self) -> Result<Option<EncodedChunk>, AacEncoderError> {
+        self.call_fdk_encode(true)
+    }
+
+    fn call_fdk_encode(&mut self, flush: bool) -> Result<Option<EncodedChunk>, AacEncoderError> {
+        let mut output = BytesMut::new();
+        let mut res;
+
+        loop {
+            // Docs of aacEncEncoded say that it should be called with numInSamples = -1 to flush the encoder.
+            let num_in_samples = match flush {
+                true => -1,
+                false => self.input_buffer.len() as c_int,
+            };
+
+            let in_args = fdk::AACENC_InArgs {
+                numInSamples: num_in_samples,
+                numAncBytes: 0,
+            };
+
+            // Calling `drain` on the input buffer will reallocate it, so we need to create the buffer descriptions right before calling `aacEncEncode`.
             // It's unsafe to use pointers obtained by calling `as_ptr()` and `as_ptr_mut()` after moving / reallocating the buffer.
-            // If I would extract creating buffer and buffer description to a separate function, move could occur unpredictably
-            // (depends on compiler), making pointers invalid and unsafe to use.
-            let mut in_buf = input_buffer.as_ptr();
+            let mut in_buf = self.input_buffer.as_ptr();
             let mut in_buf_ident: c_int = fdk::AACENC_BufferIdentifier_IN_AUDIO_DATA as c_int;
-            let mut in_buf_size: c_int = input_buffer.len() as c_int;
+            let mut in_buf_size: c_int = self.input_buffer.len() as c_int;
             let mut in_buf_el_size: c_int = mem::size_of::<i16>() as c_int;
 
             let in_desc = fdk::AACENC_BufDesc {
@@ -173,9 +212,9 @@ impl AacEncoderInner {
                 bufElSizes: &mut in_buf_el_size as *mut _,
             };
 
-            let mut out_buf = out_buffer.as_mut_ptr();
+            let mut out_buf = self.output_buffer.as_mut_ptr();
             let mut out_buf_ident: c_int = fdk::AACENC_BufferIdentifier_OUT_BITSTREAM_DATA as c_int;
-            let mut out_buf_size: c_int = out_buffer.len() as c_int;
+            let mut out_buf_size: c_int = self.output_buffer.len() as c_int;
             let mut out_buf_el_size: c_int = mem::size_of::<i16>() as c_int;
 
             let out_desc = fdk::AACENC_BufDesc {
@@ -186,25 +225,57 @@ impl AacEncoderInner {
                 bufElSizes: &mut out_buf_el_size as *mut _,
             };
 
+            let mut out_args;
             unsafe {
-                let mut out_args = mem::zeroed();
+                out_args = mem::zeroed();
 
-                check_fdk(fdk::aacEncEncode(
+                res = check(fdk::aacEncEncode(
                     self.encoder,
                     &in_desc,
                     &out_desc,
                     &in_args,
                     &mut out_args,
-                ))?;
+                ));
+            }
+
+            // Breaking here no matter what error was return seems wrong,
+            // but calling convention in documentation specifies that it should be done this way.
+            if res.is_err() {
+                break;
+            }
+
+            let consumed_samples = out_args.numInSamples as usize;
+            if consumed_samples > 0 {
+                self.input_buffer.drain(..consumed_samples);
+                self.sent_samples += consumed_samples as u128;
+            }
+
+            let encoded_bytes = out_args.numOutBytes as usize;
+            if encoded_bytes > 0 {
+                output.extend_from_slice(&self.output_buffer[..out_args.numOutBytes as usize]);
             }
         }
+        // Encode should be called after `enqueue_input`, so unwrap is safe.
+        let pts = self.start_pts.unwrap()
+            + Duration::from_secs_f64(self.sent_samples as f64 / self.sample_rate as f64);
 
-        Ok(EncodedChunk {
-            data: output.freeze(),
-            pts: todo!(),
-            dts: todo!(),
-            kind: todo!(),
-        })
+        match output.is_empty() {
+            true => Ok(None),
+            false => Ok(Some(EncodedChunk {
+                data: output.freeze(),
+                pts,
+                dts: None,
+                kind: EncodedChunkKind::Audio(AudioCodec::Aac),
+            })),
+        }
+    }
+}
+
+impl Drop for AacEncoderInner {
+    fn drop(&mut self) {
+        unsafe {
+            fdk::aacEncClose(&mut self.encoder as *mut _);
+        }
     }
 }
 
@@ -215,7 +286,7 @@ fn run_encoder_thread(
     samples_batch_receiver: Receiver<PipelineEvent<OutputSamples>>,
     packets_sender: Sender<EncoderOutputEvent>,
 ) {
-    let encoder = match AacEncoderInner::new(options, sample_rate) {
+    let mut encoder = match AacEncoderInner::new(options, sample_rate) {
         Ok(encoder) => {
             init_result_sender.send(Ok(())).unwrap();
             encoder
@@ -226,144 +297,36 @@ fn run_encoder_thread(
         }
     };
 
+    let send_encode_res = |res: Result<Option<EncodedChunk>, AacEncoderError>| match res {
+        Ok(Some(encoded_samples)) => {
+            if packets_sender
+                .send(EncoderOutputEvent::Data(encoded_samples))
+                .is_err()
+            {
+                info!("Failed to send AAC encoded samples.");
+            };
+        }
+        Ok(None) => {}
+        Err(err) => {
+            error!("Error encoding audio samples: {:?}", err);
+        }
+    };
+
     for event in samples_batch_receiver {
         match event {
-            PipelineEvent::Data(samples) => {
-                // Encode samples
-            }
+            PipelineEvent::Data(samples) => send_encode_res(encoder.encode(samples)),
             PipelineEvent::EOS => {
-                packets_sender.send(EncoderOutputEvent::AudioEOS).unwrap();
+                send_encode_res(encoder.flush());
+                if packets_sender.send(EncoderOutputEvent::AudioEOS).is_err() {
+                    info!("Failed to send EOS event.");
+                };
                 break;
             }
         }
     }
 }
 
-fn encode(
-    encoder: *mut fdk::AACENCODER,
-    samples: OutputSamples,
-    out_buffer: &mut Vec<u8>,
-) -> Result<(), AacEncoderError> {
-    let in_args = fdk::AACENC_InArgs {
-        numInSamples: samples.samples.len() as c_int,
-        numAncBytes: 0,
-    };
-
-    let input_buffer = match samples.samples {
-        AudioSamples::Mono(mono_samples) => mono_samples,
-        AudioSamples::Stereo(stereo_samples) => stereo_samples
-            .into_iter()
-            .flat_map(|(l, r)| [l, r])
-            .collect(),
-    };
-
-    // For safety, buffer descriptions are created right before calling `aacEncEncode``.
-    // It's unsafe to use pointers obtained by calling `as_ptr()` and `as_ptr_mut()` after moving / reallocating the buffer.
-    // If I would extract creating buffer and buffer description to a separate function, move could occur unpredictably
-    // (depends on compiler), making pointers invalid and unsafe to use.
-    let mut in_buf = input_buffer.as_ptr();
-    let mut in_buf_ident: c_int = fdk::AACENC_BufferIdentifier_IN_AUDIO_DATA as c_int;
-    let mut in_buf_size: c_int = input_buffer.len() as c_int;
-    let mut in_buf_el_size: c_int = mem::size_of::<i16>() as c_int;
-
-    let in_desc = fdk::AACENC_BufDesc {
-        numBufs: 1,
-        bufs: &mut in_buf as *mut _ as *mut *mut c_void,
-        bufferIdentifiers: &mut in_buf_ident as *mut _,
-        bufSizes: &mut in_buf_size as *mut _,
-        bufElSizes: &mut in_buf_el_size as *mut _,
-    };
-
-    let mut out_buf = out_buffer.as_mut_ptr();
-    let mut out_buf_ident: c_int = fdk::AACENC_BufferIdentifier_OUT_BITSTREAM_DATA as c_int;
-    let mut out_buf_size: c_int = out_buffer.len() as c_int;
-    let mut out_buf_el_size: c_int = mem::size_of::<i16>() as c_int;
-
-    let out_desc = fdk::AACENC_BufDesc {
-        numBufs: 1,
-        bufs: &mut out_buf as *mut _ as *mut *mut c_void,
-        bufferIdentifiers: &mut out_buf_ident as *mut _,
-        bufSizes: &mut out_buf_size as *mut _,
-        bufElSizes: &mut out_buf_el_size as *mut _,
-    };
-
-    unsafe {
-        let mut out_args = mem::zeroed();
-
-        check_fdk(fdk::aacEncEncode(
-            encoder,
-            &in_desc,
-            &out_desc,
-            &in_args,
-            &mut out_args,
-        ))?;
-    }
-
-    Ok(())
-}
-
-fn init_encoder(
-    options: AacEncoderOptions,
-    sample_rate: u32,
-) -> Result<(*mut fdk::AACENCODER, fdk::AACENC_InfoStruct), AacEncoderError> {
-    let mut encoder = ptr::null_mut();
-    let info;
-
-    let channels = match options.channels {
-        AudioChannels::Mono => 1,
-        AudioChannels::Stereo => 2,
-    };
-
-    unsafe {
-        check_fdk(fdk::aacEncOpen(&mut encoder as *mut _, 0, 2))?;
-        check_fdk(fdk::aacEncoder_SetParam(
-            encoder,
-            fdk::AACENC_PARAM_AACENC_BITRATEMODE,
-            5,
-        ))?;
-        check_fdk(fdk::aacEncoder_SetParam(
-            encoder,
-            fdk::AACENC_PARAM_AACENC_SAMPLERATE,
-            sample_rate,
-        ))?;
-        check_fdk(fdk::aacEncoder_SetParam(
-            encoder,
-            fdk::AACENC_PARAM_AACENC_TRANSMUX,
-            2,
-        ))?;
-        check_fdk(fdk::aacEncoder_SetParam(
-            encoder,
-            fdk::AACENC_PARAM_AACENC_SBR_MODE,
-            0,
-        ))?;
-        check_fdk(fdk::aacEncoder_SetParam(
-            encoder,
-            fdk::AACENC_PARAM_AACENC_CHANNELMODE,
-            channels,
-        ))?;
-        check_fdk(fdk::aacEncoder_SetParam(
-            encoder,
-            fdk::AACENC_PARAM_AACENC_AFTERBURNER,
-            1,
-        ))?;
-
-        check_fdk(fdk::aacEncEncode(
-            encoder,
-            ptr::null(),
-            ptr::null(),
-            ptr::null(),
-            ptr::null_mut(),
-        ))?;
-
-        let mut maybe_info = MaybeUninit::uninit();
-        check_fdk(fdk::aacEncInfo(encoder, maybe_info.as_mut_ptr()))?;
-        info = maybe_info.assume_init();
-    }
-
-    Ok((encoder, info))
-}
-
-fn check_fdk(result: fdk::AACENC_ERROR) -> Result<(), AacEncoderError> {
+fn check(result: fdk::AACENC_ERROR) -> Result<(), AacEncoderError> {
     if result == fdk::AACENC_ERROR_AACENC_OK {
         Ok(())
     } else {
