d84f25d972bafcac31b5996a4a41391f32147cb6
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-08-06 11:51:10 +0200

    added resolution settings modal

--- docs/src/components/PlaygroundRenderSettings.tsx
+++ docs/src/components/PlaygroundRenderSettings.tsx
@@ -1,31 +1,36 @@
-function PlaygroundRenderSettings({ onSubmit }: { onSubmit: () => Promise<void> }) {
+import { useState } from 'react';
+import PlaygroundRenderSettingsInputs from './PlaygroundRenderSettingsInputs';
+
+interface PlaygroundRenderSettingsProps {
+  onSubmit: () => Promise<void>;
+  onChange: (string, Resolution) => void;
+}
+
+function PlaygroundRenderSettings({ onSubmit, onChange }: PlaygroundRenderSettingsProps) {
+  const [inputsSettingsModalOpen, setInputsSettingsModalOpen] = useState(false);
+
   return (
     <div style={{ margin: '10px' }}>
-      <div className="row">
-        <div className="col">Settings:</div>
-        <div className="col">
-          <select>
-            <option value="someOption">Some option</option>
-            <option value="otherOption">Other option</option>
-          </select>
-        </div>
-        <div className="col">
-          <select>
-            <option value="someOption">Some option</option>
-            <option value="otherOption">Other option</option>
-          </select>
-        </div>
-        <div className="col">
-          <select>
-            <option value="someOption">Some option</option>
-            <option value="otherOption">Other option</option>
-          </select>
-        </div>
-        <div className="col">
-          <button className="button button--outline button--primary" onClick={onSubmit}>
-            Submit
-          </button>
-        </div>
+      <div>
+        <button
+          className="button button--outline button--secondary"
+          onClick={() => {
+            setInputsSettingsModalOpen(true);
+          }}
+          style={{ margin: '10px' }}>
+          Inputs settings
+        </button>
+        <PlaygroundRenderSettingsInputs
+          isOpen={inputsSettingsModalOpen}
+          closeModal={() => setInputsSettingsModalOpen(false)}
+          handleSettingsUpdate={onChange}
+        />
+        <button
+          className="button button--outline button--primary"
+          onClick={() => onSubmit()}
+          style={{ margin: '10px' }}>
+          Submit
+        </button>
       </div>
     </div>
   );
--- docs/src/pages/playground.tsx
+++ docs/src/pages/playground.tsx
@@ -1,21 +1,88 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-import styles from './playground.module.css';
 import { useState } from 'react';
-import PlaygroundRenderSettings from '../components/PlaygroundRenderSettings';
-import PlaygroundPreview from '../components/PlaygroundPreview';
-import PlaygroundCodeEditor from '../components/PlaygroundCodeEditor';
 import toast, { Toaster } from 'react-hot-toast';
 import { renderImage } from '../api';
+import PlaygroundCodeEditor from '../components/PlaygroundCodeEditor';
+import PlaygroundPreview from '../components/PlaygroundPreview';
+import PlaygroundRenderSettings from '../components/PlaygroundRenderSettings';
+import { AVAILABLE_RESOLUTIONS, InputResolutions, Resolution } from './Resolution';
+import styles from './playground.module.css';
 
 const INITIAL_SCENE = {
   type: 'view',
+  background_color_rgba: '#4d4d4dff',
+  children: [
+    {
+      type: 'rescaler',
+      child: { type: 'input_stream', input_id: 'input_1' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      top: 20,
+      right: 20,
+      child: { type: 'input_stream', input_id: 'input_2' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      top: 20,
+      left: 20,
+      child: { type: 'input_stream', input_id: 'input_3' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      bottom: 20,
+      left: 20,
+      child: { type: 'input_stream', input_id: 'input_4' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      bottom: 20,
+      right: 20,
+      child: { type: 'input_stream', input_id: 'input_5' },
+    },
+    {
+      type: 'rescaler',
+      width: 640,
+      height: 400,
+      top: 20,
+      right: 800,
+      child: { type: 'input_stream', input_id: 'input_6' },
+    },
+  ],
 };
+
+const INITIAL_INPUTS_RESOLUTIONS: InputResolutions = {
+  input_1: AVAILABLE_RESOLUTIONS.Resoultion1920x1080,
+  input_2: AVAILABLE_RESOLUTIONS.Resoultion1920x1080,
+  input_3: AVAILABLE_RESOLUTIONS.Resoultion1920x1080,
+  input_4: AVAILABLE_RESOLUTIONS.Resoultion1920x1080,
+  input_5: AVAILABLE_RESOLUTIONS.Resoultion1920x1080,
+  input_6: AVAILABLE_RESOLUTIONS.Resoultion1920x1080,
+};
+
 const INITIAL_SCENE_STRING = JSON.stringify(INITIAL_SCENE, null, 2);
 
 function Homepage() {
   const [scene, setScene] = useState<object | Error>(INITIAL_SCENE);
+  const [inputsResolutions, setInputsResolutions] = useState<InputResolutions>(
+    INITIAL_INPUTS_RESOLUTIONS
+  );
+  function updateInputsResolutions(inputId: string, resolution: Resolution) {
+    setInputsResolutions({
+      ...inputsResolutions,
+      [inputId]: { ...resolution },
+    });
+  }
 
   const [responseData, setResponseData] = useState({
     imageUrl: '',
@@ -31,7 +98,11 @@ function Homepage() {
       if (scene instanceof Error) {
         throw new Error(`${scene.name};\n${scene.message}`);
       }
-      const blob = await renderImage({ scene });
+      const request = {
+        scene: scene,
+        inputs: inputsResolutions,
+      };
+      const blob = await renderImage({ ...request });
       const imageObjectURL = URL.createObjectURL(blob);
 
       setResponseData({ imageUrl: imageObjectURL, errorMessage: '' });
@@ -56,7 +127,7 @@ function Homepage() {
           <PlaygroundPreview {...responseData} />
         </div>
         <div className={styles.settingsBox}>
-          <PlaygroundRenderSettings onSubmit={handleSubmit} />
+          <PlaygroundRenderSettings onSubmit={handleSubmit} onChange={updateInputsResolutions} />
         </div>
       </div>
     </div>
