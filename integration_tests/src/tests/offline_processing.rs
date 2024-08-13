3263fe76a76c57935bc6645c6512ba9806a45664
Wojciech Barczynski <barczynski.w@gmail.com>
2024-08-13 14:44:07 +0200

    Add output duration and sample rate check

--- integration_tests/src/tests/offline_processing.rs
+++ integration_tests/src/tests/offline_processing.rs
@@ -1,10 +1,15 @@
-use std::process::Command;
+use std::{
+    fs::{self},
+    path::Path,
+    process::Command,
+};
 
 use anyhow::{anyhow, Result};
 use crossbeam_channel::Sender;
 use futures_util::{SinkExt, StreamExt};
 use live_compositor::config::read_config;
 use log::info;
+use regex::Regex;
 use serde_json::json;
 use tokio_tungstenite::tungstenite;
 
@@ -16,6 +21,10 @@ const BUNNY_URL: &str =
 #[test]
 pub fn offline_processing() -> Result<()> {
     const OUTPUT_FILE: &str = "/tmp/offline_processing_output.mp4";
+    if Path::new(OUTPUT_FILE).exists() {
+        fs::remove_file(OUTPUT_FILE)?;
+    };
+
     let mut config = read_config();
     config.queue_options.ahead_of_time_processing = true;
     config.queue_options.never_drop_output_frames = true;
@@ -27,7 +36,7 @@ pub fn offline_processing() -> Result<()> {
         "input/input_1/register",
         json!({
             "type": "mp4",
-            "url":  BUNNY_URL,
+            "url": BUNNY_URL,
             "required": true
         }),
     )?;
@@ -97,12 +106,48 @@ pub fn offline_processing() -> Result<()> {
         }
     }
 
-    Command::new("ffprobe")
-        .args(["-v", "error", OUTPUT_FILE])
+    let command_output = Command::new("ffprobe")
+        .args(["-v", "error", "-show_format", OUTPUT_FILE])
         .output()
         .map_err(|e| anyhow!("Invalid mp4 file. FFprobe error: {}", e))?;
 
+    if !command_output.status.success() {
+        return Err(anyhow!(
+            "Invalid mp4 file. FFprobe error: {}",
+            String::from_utf8_lossy(&command_output.stderr)
+        ));
+    }
+
+    let output_str = String::from_utf8_lossy(&command_output.stdout);
+    let (duration, bit_rate) = extract_ffprobe_info(&output_str)?;
+
+    if !(1.9..=2.1).contains(&duration) {
+        return Err(anyhow!("Invalid duration: {}", duration));
+    }
+    if !(950_000..=980_000).contains(&bit_rate) {
+        return Err(anyhow!("Invalid bit rate: {}", bit_rate));
+    }
+
     Ok(())
+}
+
+fn extract_ffprobe_info(output: &str) -> Result<(f64, u64)> {
+    let re_duration = Regex::new(r"duration=(\d+\.\d+)").unwrap();
+    let re_bit_rate = Regex::new(r"bit_rate=(\d+)").unwrap();
+
+    let duration: f64 = re_duration
+        .captures(output)
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str().parse().unwrap_or(0.0))
+        .ok_or_else(|| anyhow!("Failed to extract duration"))?;
+
+    let bit_rate: u64 = re_bit_rate
+        .captures(output)
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str().parse().unwrap_or(0))
+        .ok_or_else(|| anyhow!("Failed to extract bit rate"))?;
+
+    Ok((duration, bit_rate))
 }
 
 pub fn start_server_msg_listener(port: u16, event_sender: Sender<tungstenite::Message>) {
