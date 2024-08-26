7d5d6d02ac600aa5eb23299a2c5ec7f70e126430
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-08-26 13:35:45 +0200

    Added settings panel (#663)

---
+++ docs/src/components/PlaygroundSettings.tsx
@@ -0,0 +1,123 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import ReactModal from 'react-modal';
+import { Tooltip } from 'react-tooltip';
+import { InputResolution, InputsSettings } from '../resolution';
+import styles from './PlaygroundSettings.module.css';
+import SettingsInputs from './PlaygroundSettingsInputs';
+
+interface PlaygroundSettingsProps {
+  onSubmit: () => Promise<void>;
+  onChange: (input_id: string, resolution: InputResolution) => void;
+  inputsSettings: InputsSettings;
+  readyToSubmit: boolean;
+}
+
+export default function PlaygroundSettings({
+  onSubmit,
+  onChange,
+  inputsSettings,
+  readyToSubmit,
+}: PlaygroundSettingsProps) {
+  const [inputsSettingsModalOpen, setInputsSettingsModalOpen] = useState(false);
+
+  return (
+    <div className={styles.settingsPanel}>
+      <div className={styles.settings}>
+        <div className={styles.cardsContainer}>
+          <Card
+            title="Inputs resolutions"
+            subtitle="settings"
+            onClick={() => setInputsSettingsModalOpen(true)}
+          />
+
+          <Card
+            title="Images"
+            subtitle="preview"
+            onClick={() => setInputsSettingsModalOpen(true)}
+          />
+
+          <Card
+            title="Shaders"
+            subtitle="preview"
+            onClick={() => setInputsSettingsModalOpen(true)}
+          />
+        </div>
+      </div>
+
+      <ReactModal
+        isOpen={inputsSettingsModalOpen}
+        onRequestClose={() => setInputsSettingsModalOpen(false)}
+        overlayClassName={styles.modalOverlay}
+        className={styles.modalContent}
+        ariaHideApp={false}>
+        <SettingsInputs handleSettingsUpdate={onChange} inputsSettings={inputsSettings} />
+      </ReactModal>
+
+      <div className={styles.bottomContainer}>
+        <OutputResolution />
+        <SubmitButton onSubmit={onSubmit} readyToSubmit={readyToSubmit} />
+      </div>
+    </div>
+  );
+}
+
+type CardProps = {
+  title: string;
+  subtitle: string;
+  onClick: () => void;
+};
+
+function Card(props: CardProps) {
+  return (
+    <div className={clsx('card', styles.card, styles.hoverPrimary)} onClick={props.onClick}>
+      <div className={styles.cardTitle}>{props.title}</div>
+      <div className={styles.cardSubtitle}>{props.subtitle}</div>
+    </div>
+  );
+}
+
+function SubmitButton({
+  onSubmit,
+  readyToSubmit,
+}: {
+  onSubmit: () => Promise<void>;
+  readyToSubmit: boolean;
+}) {
+  const tooltipStyle = {
+    color: 'var(--ifm-font-color-base-inverse)',
+    backgroundColor: 'var(--ifm-color-emphasis-700)',
+  };
+  return (
+    <div
+      data-tooltip-id={readyToSubmit ? null : 'disableSubmit'}
+      data-tooltip-content={readyToSubmit ? null : 'Invalid scene provided!'}
+      data-tooltip-place={readyToSubmit ? null : 'top'}>
+      <button
+        className={`${styles.submitButton} ${styles.hoverPrimary} ${
+          readyToSubmit ? styles.submitButtonActive : styles.submitButtonInactive
+        }`}
+        onClick={onSubmit}>
+        Submit
+      </button>
+      <Tooltip id="disableSubmit" style={tooltipStyle} opacity={1} />
+    </div>
+  );
+}
+
+function OutputResolution() {
+  return (
+    <div className={styles.outputResolutionsContainer}>
+      <div className={styles.outputResolutionLabel}>Output resolution:</div>
+
+      <select className={styles.outputResolutionSelect}>
+        <option value="Resoultion1920x1080">[16:9] 1920x1080</option>
+        <option value="Resoultion1080x1920">[9:16] 1080x1920</option>
+        <option value="Resoultion854x480">[16:9] 854x480</option>
+        <option value="Resoultion480x854">[9:16] 480x854</option>
+        <option value="Resoultion1440x1080">[4:3] 1440x1080</option>
+        <option value="Resoultion1080x1440">[3:4] 1080x1440</option>
+      </select>
+    </div>
+  );
+}
--- docs/src/pages/playground.tsx
+++ docs/src/pages/playground.tsx
@@ -1,19 +1,66 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-
 import styles from './playground.module.css';
 import React, { useEffect, useState } from 'react';
-import PlaygroundRenderSettings from '../components/PlaygroundRenderSettings';
 import PlaygroundPreview from '../components/PlaygroundPreview';
 import PlaygroundCodeEditor from '../components/PlaygroundCodeEditor';
 import toast, { Toaster } from 'react-hot-toast';
-import { ApiError, renderImage } from '../api';
 import 'react-tooltip/dist/react-tooltip.css';
+import { ApiError, renderImage } from '../api';
+import PlaygroundSettings from '../components/PlaygroundSettings';
+import { InputResolution, inputResolutionsToResolutions, InputsSettings } from '../resolution';
 import PlaygroundReactEditor from '../components/PlaygroundReactEditor';
 import executeTypescriptCode from '../executeTypescriptCode';
 
 const INITIAL_SCENE = {
   type: 'view',
+  background_color_rgba: '#4d4d4dff',
+  children: [
+    {
+      type: 'rescaler',
+      child: { type: 'input_stream', input_id: 'input_1' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      top: 20,
+      right: 20,
+      child: { type: 'input_stream', input_id: 'input_2' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      top: 20,
+      left: 20,
+      child: { type: 'input_stream', input_id: 'input_3' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      bottom: 20,
+      left: 20,
+      child: { type: 'input_stream', input_id: 'input_4' },
+    },
+    {
+      type: 'rescaler',
+      width: 320,
+      height: 180,
+      bottom: 20,
+      right: 20,
+      child: { type: 'input_stream', input_id: 'input_5' },
+    },
+    {
+      type: 'rescaler',
+      width: 640,
+      height: 400,
+      top: 20,
+      right: 800,
+      child: { type: 'input_stream', input_id: 'input_6' },
+    },
+  ],
 };
 
 const INITIAL_REACT_CODE = [
@@ -37,6 +84,20 @@ function Homepage() {
   const [scene, setScene] = useState<object | Error>(INITIAL_SCENE);
   const [code, setCode] = useState<string>(INITIAL_REACT_CODE);
   const [showReactEditor, setShowReactEditor] = useState<boolean>(false);
+  const [inputResolutions, setInputResolutions] = useState<InputsSettings>({
+    input_1: InputResolution.Resoultion1920x1080,
+    input_2: InputResolution.Resoultion1920x1080,
+    input_3: InputResolution.Resoultion1920x1080,
+    input_4: InputResolution.Resoultion1920x1080,
+    input_5: InputResolution.Resoultion1920x1080,
+    input_6: InputResolution.Resoultion1920x1080,
+  });
+  function updateInputResolutions(inputId: string, resolution: InputResolution) {
+    setInputResolutions({
+      ...inputResolutions,
+      [inputId]: resolution,
+    });
+  }
 
   const [responseData, setResponseData] = useState({
     imageUrl: '',
@@ -55,7 +116,11 @@ function Homepage() {
         if (scene instanceof Error) {
           throw new Error(`${scene.name};\n${scene.message}`);
         }
-        const blob = await renderImage({ scene });
+        const request = {
+          scene: scene,
+          inputs: inputResolutionsToResolutions(inputResolutions),
+        };
+        const blob = await renderImage({ ...request });
         const imageObjectURL = URL.createObjectURL(blob);
 
         setResponseData({ imageUrl: imageObjectURL, errorMessage: '' });
@@ -93,9 +158,11 @@ function Homepage() {
           <PlaygroundPreview {...responseData} />
         </div>
         <div className={styles.settingsBox}>
-          <PlaygroundRenderSettings
+          <PlaygroundSettings
             onSubmit={handleSubmit}
             readyToSubmit={!(scene instanceof Error) || showReactEditor}
+            onChange={updateInputResolutions}
+            inputsSettings={inputResolutions}
           />
         </div>
       </div>
