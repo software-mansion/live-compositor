fa1372ea3fe127f88c61434efa03af316f55a999
Wojciech Barczynski <barczynski.w@gmail.com>
2024-08-29 23:31:20 +0200

    Add MP4 loop version docs info

--- compositor_api/src/types/register_input.rs
+++ compositor_api/src/types/register_input.rs
@@ -37,6 +37,7 @@ pub struct Mp4Input {
     /// Path to the MP4 file.
     pub path: Option<String>,
     /// (**default=`false`**) If input should be played in the loop.
+    /// Added in v0.4.0.
     #[serde(rename = "loop")]
     pub should_loop: Option<bool>,
     /// (**default=`false`**) If input is required and frames are not processed
