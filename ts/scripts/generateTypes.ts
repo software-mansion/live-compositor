fe558d545bf80816e86b0fb6aa20b5f72333e088
noituri <mikolaj.radkowski@protonmail.com>
2024-09-04 16:36:42 +0200

    CI

--- ts/scripts/generateTypes.ts
+++ ts/scripts/generateTypes.ts
@@ -6,7 +6,6 @@ async function generateTypes() {
   const schemaPath = path.resolve(__dirname, '../../schemas/api_types.schema.json');
   const outputPaths = [
     path.resolve(__dirname, '../live-compositor/src/api.generated.ts'),
-    path.resolve(__dirname, '../@live-compositor/browser-render/src/api.generated.ts')
   ];
 
   const typesTs = await compileFromFile(schemaPath, {
