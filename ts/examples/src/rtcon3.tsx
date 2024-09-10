f1610909e4909b1b8c23ce8a07fb1d6392b96b2a
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-09-10 12:41:08 +0200

    example 3

--- ts/examples/src/rtcon3.tsx
+++ ts/examples/src/rtcon3.tsx
@@ -1,22 +1,59 @@
 import LiveCompositor from '@live-compositor/node';
 import { Text, InputStream, Tiles, Rescaler, View, useInputStreams, Image } from 'live-compositor';
-import { downloadAllAssets, ffmpegSendVideoFromMp4, sleep } from './utils';
+import { downloadAllAssets, sleep } from './utils';
 import path from 'path';
 import fs from 'fs-extra';
+import { useState, useEffect } from 'react';
 
 function App() {
   const inputs = useInputStreams();
   return (
-    <Tiles transition={{ durationMs: 400 }}>
-      {Object.values(inputs).map(({ inputId, videoState }) =>
-        videoState === 'playing' ? (
-          <InputTile key={inputId} inputId={inputId} />
-        ) : (
-          <Image key={inputId} imageId="spinner" />
-        )
-      )}
-    </Tiles>
+    <View>
+      <Tiles transition={{ durationMs: 400 }}>
+        {Object.values(inputs).map(({ inputId, videoState }) =>
+          videoState === 'playing' ? (
+            <InputTile key={inputId} inputId={inputId} />
+          ) : (
+            <Image key={inputId} imageId="spinner" />
+          )
+        )}
+      </Tiles>
+      <NewInputToast />
+    </View>
   );
+}
+
+function NewInputToast() {
+  const inputs = useInputStreams();
+  const [current, setCurrent] = useState<string | null>(null);
+  const [prevStreams, setPrevStreams] = useState<string[]>([]);
+
+  useEffect(() => {
+    const currentStreams = Object.keys(inputs);
+    const newStream = currentStreams.find(stream => !prevStreams.includes(stream));
+    setPrevStreams(currentStreams);
+    if (newStream) {
+      setCurrent(newStream);
+    }
+  }, [inputs]);
+
+  useEffect(() => {
+    if (!setCurrent) {
+      return;
+    }
+    const timeout = setTimeout(() => {
+      setCurrent(null);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [current]);
+
+  return current ? (
+    <View top={0} left={0} backgroundColor="#FFFFFFFF" height={50}>
+      <Text fontSize={40} color="#FF0000">
+        Connecting new stream: {current}
+      </Text>
+    </View>
+  ) : undefined;
 }
 
 function InputTile({ inputId }: { inputId: string }) {
@@ -56,7 +93,7 @@ async function run() {
 
   await compositor.registerOutput('output', {
     type: 'mp4',
-    serverPath: path.join(__dirname, '../.workingdir/rtcon2.mp4'),
+    serverPath: path.join(__dirname, '../.workingdir/rtcon3.mp4'),
     video: {
       encoder: VIDEO_ENCODER_OPTS,
       resolution: RESOLUTION,
@@ -76,18 +113,13 @@ async function run() {
   });
   await compositor.start();
 
-  await sleep(3_000);
-  // using stream instead of mp4 so there is delay between registration and starting stream
+  await sleep(4_000);
   await compositor.registerInput('input_2', {
-    type: 'rtp_stream',
-    port: 8006,
-    video: {
-      decoder: 'ffmpeg_h264',
-    },
+    type: 'mp4',
+    serverPath: path.join(__dirname, '../.assets/ElephantsDream.mp4'),
   });
-  ffmpegSendVideoFromMp4(8006, path.join(__dirname, '../.assets/ElephantsDream.mp4'));
 
-  await sleep(5_000);
+  await sleep(10_000);
   await compositor.unregisterOutput('output');
   await sleep(1_000);
   process.exit(0);
