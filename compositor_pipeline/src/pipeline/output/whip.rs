0789069fee0da2470c965c55b8bb5ab0a46c12fc
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2024-10-24 11:43:42 +0200

    error handling

--- compositor_pipeline/src/pipeline/output/whip.rs
+++ compositor_pipeline/src/pipeline/output/whip.rs
@@ -1,37 +1,26 @@
 use compositor_render::OutputId;
 use crossbeam_channel::{Receiver, Sender};
-use payloader::Payload;
-use reqwest::{header::HeaderMap, Url};
+use establish_peer_connection::connect;
+use init_peer_connection::init_pc;
+use packet_stream::PacketStream;
+use payloader::{Payload, Payloader, PayloadingError};
+use reqwest::{Method, StatusCode, Url};
 use std::sync::{atomic::AtomicBool, Arc};
-use tracing::{debug, error, info, span, Level};
+use tracing::{debug, error, span, Level};
+use url::ParseError;
 use webrtc::{
-    api::{
-        interceptor_registry::register_default_interceptors,
-        media_engine::{MediaEngine, MIME_TYPE_H264, MIME_TYPE_OPUS},
-        APIBuilder,
-    },
-    ice_transport::{ice_connection_state::RTCIceConnectionState, ice_server::RTCIceServer},
-    interceptor::registry::Registry,
-    peer_connection::{
-        configuration::RTCConfiguration, sdp::session_description::RTCSessionDescription,
-        RTCPeerConnection,
-    },
-    rtcp::payload_feedbacks::picture_loss_indication::PictureLossIndication,
-    rtp_transceiver::{
-        rtp_codec::{RTCRtpCodecCapability, RTCRtpCodecParameters, RTPCodecType},
-        rtp_transceiver_direction::RTCRtpTransceiverDirection,
-    },
+    peer_connection::RTCPeerConnection,
     track::track_local::{track_local_static_rtp::TrackLocalStaticRTP, TrackLocalWriter},
 };
 
 use crate::{
     error::OutputInitError,
     event::Event,
-    pipeline::{types::EncoderOutputEvent, AudioCodec, PipelineCtx, VideoCodec},
+    pipeline::{AudioCodec, EncoderOutputEvent, PipelineCtx, VideoCodec},
 };
 
-use self::{packet_stream::PacketStream, payloader::Payloader};
-
+mod establish_peer_connection;
+mod init_peer_connection;
 mod packet_stream;
 mod payloader;
 
@@ -51,8 +40,52 @@ pub struct WhipSenderOptions {
 
 #[derive(Debug, thiserror::Error)]
 pub enum WhipError {
-    #[error("Missing location header in WHIP response")]
+    #[error("Bad status in WHIP response\nStatus: {0}\nBody: {1}")]
+    BadStatus(StatusCode, String),
+
+    #[error("WHIP request failed!\nMethod: {0}\nURL: {1}")]
+    RequestFailed(Method, Url),
+
+    #[error(
+        "Unable to get location endpoint, check correctness of WHIP endpoint and your Bearer token"
+    )]
     MissingLocationHeader,
+
+    #[error("Invalid endpoint URL: {1}")]
+    InvalidEndpointUrl(#[source] ParseError, String),
+
+    #[error("Missing Host in endpoint URL")]
+    MissingHost,
+
+    #[error("Missing port in endpoint URL")]
+    MissingPort,
+
+    #[error("Failed to create RTC session description: {0}")]
+    RTCSessionDescriptionError(webrtc::Error),
+
+    #[error("Failed to set local description: {0}")]
+    LocalDescriptionError(webrtc::Error),
+
+    #[error("Failed to set remote description: {0}")]
+    RemoteDescriptionError(webrtc::Error),
+
+    #[error("Failed to parse {0} response body: {1}")]
+    BodyParsingError(String, reqwest::Error),
+
+    #[error("Failed to create offer: {0}")]
+    OfferCreationError(webrtc::Error),
+
+    #[error(transparent)]
+    PeerConnectionInitError(#[from] webrtc::Error),
+
+    #[error("Failed to convert ICE candidate to JSON: {0}")]
+    IceCandidateToJsonError(webrtc::Error),
+
+    #[error(transparent)]
+    SerdeJsonError(#[from] serde_json::Error),
+
+    #[error(transparent)]
+    PayloadingError(#[from] PayloadingError),
 }
 
 impl WhipSender {
@@ -121,7 +154,20 @@ fn start_whip_sender_thread(
 ) {
     tokio_rt.block_on(async {
         let client = reqwest::Client::new();
-        let (peer_connection, video_track, audio_track) = init_pc().await;
+        let peer_connection: Arc<RTCPeerConnection>;
+        let video_track: Arc<TrackLocalStaticRTP>;
+        let audio_track: Arc<TrackLocalStaticRTP>;
+        match init_pc().await {
+            Ok((pc, video, audio)) => {
+                peer_connection = pc;
+                video_track = video;
+                audio_track = audio;
+            }
+            Err(err) => {
+                error!("Error occured while initializing peer connection: {err}");
+                return;
+            }
+        }
         let whip_session_url = match connect(
             peer_connection,
             endpoint_url,
@@ -134,7 +180,10 @@ fn start_whip_sender_thread(
         .await
         {
             Ok(val) => val,
-            Err(_) => return,
+            Err(err) => {
+                error!("{err}");
+                return;
+            }
         };
 
         for chunk in packet_stream {
@@ -150,235 +199,28 @@ fn start_whip_sender_thread(
             };
 
             match chunk {
-                Payload::Video(bytes) => {
-                    if video_track.write(&bytes).await.is_err() {
-                        error!("Error occurred while writing to video track for session");
+                Payload::Video(video_payload) => match video_payload {
+                    Ok(video_bytes) => {
+                        if video_track.write(&video_bytes).await.is_err() {
+                            error!("Error occurred while writing to video track for session");
+                        }
                     }
-                }
-                Payload::Audio(bytes) => {
-                    if audio_track.write(&bytes).await.is_err() {
-                        error!("Error occurred while writing to audio track for session");
+                    Err(err) => {
+                        error!("Error while reading video bytes: {err}");
                     }
-                }
+                },
+                Payload::Audio(audio_payload) => match audio_payload {
+                    Ok(audio_bytes) => {
+                        if audio_track.write(&audio_bytes).await.is_err() {
+                            error!("Error occurred while writing to video track for session");
+                        }
+                    }
+                    Err(err) => {
+                        error!("Error while reading audio bytes: {err}");
+                    }
+                },
             }
         }
         let _ = client.delete(whip_session_url).send().await;
     });
-}
-
-async fn init_pc() -> (
-    Arc<RTCPeerConnection>,
-    Arc<TrackLocalStaticRTP>,
-    Arc<TrackLocalStaticRTP>,
-) {
-    let mut m = MediaEngine::default();
-    m.register_default_codecs().unwrap();
-    m.register_codec(
-        RTCRtpCodecParameters {
-            capability: RTCRtpCodecCapability {
-                mime_type: MIME_TYPE_H264.to_owned(),
-                clock_rate: 90000,
-                channels: 0,
-                sdp_fmtp_line: "".to_owned(),
-                rtcp_feedback: vec![],
-            },
-            payload_type: 96,
-            ..Default::default()
-        },
-        RTPCodecType::Video,
-    )
-    .unwrap();
-    m.register_codec(
-        RTCRtpCodecParameters {
-            capability: RTCRtpCodecCapability {
-                mime_type: MIME_TYPE_OPUS.to_owned(),
-                clock_rate: 48000,
-                channels: 2,
-                sdp_fmtp_line: "".to_owned(),
-                rtcp_feedback: vec![],
-            },
-            payload_type: 111,
-            ..Default::default()
-        },
-        RTPCodecType::Audio,
-    )
-    .unwrap();
-    let mut registry = Registry::new();
-    registry = register_default_interceptors(registry, &mut m).unwrap();
-    let api = APIBuilder::new()
-        .with_media_engine(m)
-        .with_interceptor_registry(registry)
-        .build();
-
-    let config = RTCConfiguration {
-        ice_servers: vec![RTCIceServer {
-            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
-            ..Default::default()
-        }],
-        ..Default::default()
-    };
-    let peer_connection = Arc::new(api.new_peer_connection(config).await.unwrap());
-    let video_track = Arc::new(TrackLocalStaticRTP::new(
-        RTCRtpCodecCapability {
-            mime_type: MIME_TYPE_H264.to_owned(),
-            ..Default::default()
-        },
-        "video".to_owned(),
-        "webrtc-rs".to_owned(),
-    ));
-    let audio_track = Arc::new(TrackLocalStaticRTP::new(
-        RTCRtpCodecCapability {
-            mime_type: MIME_TYPE_OPUS.to_owned(),
-            ..Default::default()
-        },
-        "audio".to_owned(),
-        "webrtc-rs".to_owned(),
-    ));
-    let _ = peer_connection.add_track(video_track.clone()).await;
-    let _ = peer_connection.add_track(audio_track.clone()).await;
-    let transceivers = peer_connection.get_transceivers().await;
-    for transceiver in transceivers {
-        transceiver
-            .set_direction(RTCRtpTransceiverDirection::Sendonly)
-            .await;
-    }
-    (peer_connection, video_track, audio_track)
-}
-
-async fn connect(
-    peer_connection: Arc<RTCPeerConnection>,
-    endpoint_url: String,
-    bearer_token: Option<String>,
-    should_close: Arc<AtomicBool>,
-    tokio_rt: Arc<tokio::runtime::Runtime>,
-    client: reqwest::Client,
-    request_keyframe_sender: Option<Sender<()>>,
-) -> Result<Url, WhipError> {
-    peer_connection.on_ice_connection_state_change(Box::new(
-        move |connection_state: RTCIceConnectionState| {
-            debug!("Connection State has changed {connection_state}");
-            if connection_state == RTCIceConnectionState::Connected {
-                debug!("ice connected");
-            } else if connection_state == RTCIceConnectionState::Failed {
-                debug!("Done writing media files");
-                should_close.store(true, std::sync::atomic::Ordering::Relaxed);
-            }
-            Box::pin(async {})
-        },
-    ));
-
-    if let Some(keyframe_sender) = request_keyframe_sender {
-        let senders = peer_connection.get_senders().await;
-        for sender in senders {
-            let keyframe_sender_clone = keyframe_sender.clone();
-            tokio_rt.spawn(async move {
-                loop {
-                    let packets = &sender.read_rtcp().await.unwrap().0;
-                    for packet in packets {
-                        if packet
-                            .as_any()
-                            .downcast_ref::<PictureLossIndication>()
-                            .is_some()
-                        {
-                            if let Err(err) = keyframe_sender_clone.send(()) {
-                                debug!(%err, "Failed to send keyframe request to the encoder.");
-                            };
-                        }
-                    }
-                }
-            });
-        }
-    }
-
-    let offer = peer_connection.create_offer(None).await.unwrap();
-
-    info!("[WHIP] endpoint url: {}", endpoint_url);
-
-    let mut header_map = HeaderMap::new();
-    header_map.append("Content-Type", "application/sdp".parse().unwrap());
-
-    let bearer_token = bearer_token.map(Arc::new);
-
-    if let Some(token) = bearer_token.clone() {
-        header_map.append("Authorization", format!("Bearer {token}").parse().unwrap());
-    }
-
-    let response = client
-        .post(endpoint_url.clone())
-        .headers(header_map)
-        .body(offer.sdp.clone())
-        .send()
-        .await
-        .unwrap();
-
-    info!("[WHIP] response: {:?}", &response);
-
-    let parsed_endpoint_url = Url::parse(&endpoint_url).unwrap();
-
-    let location_url_path = response.headers()
-        .get("location")
-        .and_then(|url| url.to_str().ok())
-        .ok_or_else(|| {
-            error!("Unable to get location endpoint, check correctness of WHIP endpoint and your Bearer token");
-            WhipError::MissingLocationHeader
-        })?;
-
-    let location_url = Url::try_from(
-        format!(
-            "{}://{}:{}{}",
-            parsed_endpoint_url.scheme(),
-            parsed_endpoint_url.host_str().unwrap(),
-            parsed_endpoint_url.port().unwrap(),
-            location_url_path
-        )
-        .as_str(),
-    )
-    .unwrap();
-
-    let answer = response.bytes().await.unwrap();
-    peer_connection.set_local_description(offer).await.unwrap();
-
-    peer_connection
-        .set_remote_description(
-            RTCSessionDescription::answer(std::str::from_utf8(&answer).unwrap().to_string())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    let client = Arc::new(client);
-
-    let location1: Url = location_url.clone();
-
-    peer_connection.on_ice_candidate(Box::new(move |candidate| {
-        if let Some(candidate) = candidate {
-            let client_clone = client.clone();
-            let location2 = location1.clone();
-            let bearer_token1 = bearer_token.clone();
-            tokio_rt.spawn(async move {
-                let ice_candidate = candidate.to_json().unwrap();
-
-                let mut header_map = HeaderMap::new();
-                header_map.append(
-                    "Content-Type",
-                    "application/trickle-ice-sdpfrag".parse().unwrap(),
-                );
-
-                if let Some(token) = bearer_token1 {
-                    header_map.append("Authorization", format!("Bearer {token}").parse().unwrap());
-                }
-
-                let _ = client_clone
-                    .patch(location2)
-                    .headers(header_map)
-                    .body(serde_json::to_string(&ice_candidate).unwrap())
-                    .send()
-                    .await
-                    .unwrap();
-            });
-        }
-        Box::pin(async {})
-    }));
-
-    Ok(location_url.clone())
 }
---
+++ compositor_pipeline/src/pipeline/output/whip/establish_peer_connection.rs
@@ -0,0 +1,187 @@
+use crossbeam_channel::Sender;
+
+use super::WhipError;
+use reqwest::{header::HeaderMap, Method, StatusCode, Url};
+use std::sync::{atomic::AtomicBool, Arc};
+use tracing::{debug, info};
+use webrtc::{
+    ice_transport::ice_connection_state::RTCIceConnectionState,
+    peer_connection::{sdp::session_description::RTCSessionDescription, RTCPeerConnection},
+    rtcp::payload_feedbacks::picture_loss_indication::PictureLossIndication,
+};
+
+pub async fn connect(
+    peer_connection: Arc<RTCPeerConnection>,
+    endpoint_url: String,
+    bearer_token: Option<String>,
+    should_close: Arc<AtomicBool>,
+    tokio_rt: Arc<tokio::runtime::Runtime>,
+    client: reqwest::Client,
+    request_keyframe_sender: Option<Sender<()>>,
+) -> Result<Url, WhipError> {
+    peer_connection.on_ice_connection_state_change(Box::new(
+        move |connection_state: RTCIceConnectionState| {
+            debug!("Connection State has changed {connection_state}");
+            if connection_state == RTCIceConnectionState::Connected {
+                debug!("ice connected");
+            } else if connection_state == RTCIceConnectionState::Failed {
+                debug!("Done writing media files");
+                should_close.store(true, std::sync::atomic::Ordering::Relaxed);
+            }
+            Box::pin(async {})
+        },
+    ));
+
+    if let Some(keyframe_sender) = request_keyframe_sender {
+        let senders = peer_connection.get_senders().await;
+        for sender in senders {
+            let keyframe_sender_clone = keyframe_sender.clone();
+            tokio_rt.spawn(async move {
+                loop {
+                    if let Ok((packets, _)) = &sender.read_rtcp().await {
+                        for packet in packets {
+                            if packet
+                                .as_any()
+                                .downcast_ref::<PictureLossIndication>()
+                                .is_some()
+                            {
+                                if let Err(err) = keyframe_sender_clone.send(()) {
+                                    debug!(%err, "Failed to send keyframe request to the encoder.");
+                                };
+                            }
+                        }
+                    } else {
+                        debug!("Failed to read RTCP packets from the sender.");
+                    }
+                }
+            });
+        }
+    }
+
+    let offer = peer_connection
+        .create_offer(None)
+        .await
+        .map_err(WhipError::OfferCreationError)?;
+
+    let endpoint_url = Url::parse(&endpoint_url)
+        .map_err(|e| WhipError::InvalidEndpointUrl(e, endpoint_url.clone()))?;
+
+    info!("[WHIP] endpoint url: {}", endpoint_url);
+
+    let mut header_map = HeaderMap::new();
+    header_map.append("Content-Type", "application/sdp".parse().unwrap());
+
+    let bearer_token = bearer_token.map(Arc::new);
+
+    if let Some(token) = bearer_token.clone() {
+        header_map.append("Authorization", format!("Bearer {token}").parse().unwrap());
+    }
+
+    let response = match client
+        .post(endpoint_url.clone())
+        .headers(header_map)
+        .body(offer.sdp.clone())
+        .send()
+        .await
+    {
+        Ok(res) => res,
+        Err(_) => return Err(WhipError::RequestFailed(Method::POST, endpoint_url)),
+    };
+
+    if response.status() >= StatusCode::BAD_REQUEST {
+        let status = response.status();
+        let answer = response
+            .text()
+            .await
+            .map_err(|e| WhipError::BodyParsingError("sdp offer".to_string(), e))?;
+        return Err(WhipError::BadStatus(status, answer));
+    }
+
+    info!("[WHIP] response: {:?}", &response);
+
+    let location_url_path = response
+        .headers()
+        .get("location")
+        .and_then(|url| url.to_str().ok())
+        .ok_or_else(|| WhipError::MissingLocationHeader)?;
+
+    let scheme = endpoint_url.scheme();
+    let host = endpoint_url
+        .host_str()
+        .ok_or_else(|| WhipError::MissingHost)?;
+
+    let port = endpoint_url.port().ok_or_else(|| WhipError::MissingPort)?;
+
+    let formatted_url = format!("{}://{}:{}{}", scheme, host, port, location_url_path);
+
+    let location_url = Url::try_from(formatted_url.as_str())
+        .map_err(|e| WhipError::InvalidEndpointUrl(e, formatted_url))?;
+
+    peer_connection
+        .set_local_description(offer)
+        .await
+        .map_err(WhipError::LocalDescriptionError)?;
+
+    let answer = response
+        .text()
+        .await
+        .map_err(|e| WhipError::BodyParsingError("sdp offer".to_string(), e))?;
+
+    let rtc_answer =
+        RTCSessionDescription::answer(answer).map_err(WhipError::RTCSessionDescriptionError)?;
+
+    peer_connection
+        .set_remote_description(rtc_answer)
+        .await
+        .map_err(WhipError::RemoteDescriptionError)?;
+
+    let client = Arc::new(client);
+
+    let location1: Url = location_url.clone();
+
+    peer_connection.on_ice_candidate(Box::new(move |candidate| {
+        if let Some(candidate) = candidate {
+            let client_clone = client.clone();
+            let location2 = location1.clone();
+            let bearer_token1 = bearer_token.clone();
+            tokio_rt.spawn(async move {
+                let ice_candidate = candidate
+                    .to_json()
+                    .map_err(WhipError::IceCandidateToJsonError)?;
+
+                let mut header_map = HeaderMap::new();
+                header_map.append(
+                    "Content-Type",
+                    "application/trickle-ice-sdpfrag".parse().unwrap(),
+                );
+
+                if let Some(token) = bearer_token1 {
+                    header_map.append("Authorization", format!("Bearer {token}").parse().unwrap());
+                }
+
+                let response = match client_clone
+                    .patch(location2.clone())
+                    .headers(header_map)
+                    .body(serde_json::to_string(&ice_candidate)?)
+                    .send()
+                    .await
+                {
+                    Ok(res) => res,
+                    Err(_) => return Err(WhipError::RequestFailed(Method::PATCH, location2)),
+                };
+
+                if response.status() >= StatusCode::BAD_REQUEST {
+                    let status = response.status();
+                    let body_str = response.text().await.map_err(|e| {
+                        WhipError::BodyParsingError("Trickle ICE patch".to_string(), e)
+                    })?;
+                    return Err(WhipError::BadStatus(status, body_str));
+                };
+                Ok(response)
+            });
+        }
+        Box::pin(async {})
+    }));
+
+    Ok(location_url.clone())
+}
