568d9217d97728179f50ad04a5df3de8a44270a1
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-11-14 10:52:00 +0100

    [tests] Add some tests that verify YUV conversion

--- src/snapshot_tests/utils.rs
+++ src/snapshot_tests/utils.rs
@@ -1,25 +1,36 @@
 use core::panic;
 use std::{
+    io::Write,
     sync::{Arc, OnceLock},
     time::Duration,
 };
 
+use bytes::BufMut;
 use compositor_render::{
     create_wgpu_ctx, web_renderer, Frame, FrameData, Framerate, Renderer, RendererOptions,
     WgpuFeatures, YuvPlanes,
 };
+use crossbeam_channel::bounded;
+use tracing::error;
 
 pub const SNAPSHOTS_DIR_NAME: &str = "snapshot_tests/snapshots/render_snapshots";
 
 pub(super) fn frame_to_rgba(frame: &Frame) -> Vec<u8> {
-    let FrameData::PlanarYuv420(YuvPlanes {
+    match &frame.data {
+        FrameData::PlanarYuv420(planes) => yuv_frame_to_rgba(frame, planes),
+        FrameData::PlanarYuvJ420(_) => panic!("unsupported"),
+        FrameData::InterleavedYuv422(_) => panic!("unsupported"),
+        FrameData::Rgba8UnormWgpuTexture(texture) => read_rgba_texture(texture).to_vec(),
+        FrameData::Nv12WgpuTexture(_) => panic!("unsupported"),
+    }
+}
+
+pub(super) fn yuv_frame_to_rgba(frame: &Frame, planes: &YuvPlanes) -> Vec<u8> {
+    let YuvPlanes {
         y_plane,
         u_plane,
         v_plane,
-    }) = &frame.data
-    else {
-        panic!("Wrong pixel format")
-    };
+    } = planes;
 
     // Renderer can sometimes produce resolution that is not dividable by 2
     let corrected_width = frame.resolution.width - (frame.resolution.width % 2);
@@ -46,11 +57,13 @@ pub(super) fn frame_to_rgba(frame: &Frame) -> Vec<u8> {
     rgba_data
 }
 
-pub(super) fn create_renderer() -> Renderer {
+fn get_wgpu_ctx() -> (Arc<wgpu::Device>, Arc<wgpu::Queue>) {
     static CTX: OnceLock<(Arc<wgpu::Device>, Arc<wgpu::Queue>)> = OnceLock::new();
-    let wgpu_ctx =
-        CTX.get_or_init(|| create_wgpu_ctx(false, Default::default(), Default::default()).unwrap());
+    CTX.get_or_init(|| create_wgpu_ctx(false, Default::default(), Default::default()).unwrap())
+        .clone()
+}
 
+pub(super) fn create_renderer() -> Renderer {
     let (renderer, _event_loop) = Renderer::new(RendererOptions {
         web_renderer: web_renderer::WebRendererInitOptions {
             enable: false,
@@ -60,9 +73,98 @@ pub(super) fn create_renderer() -> Renderer {
         framerate: Framerate { num: 30, den: 1 },
         stream_fallback_timeout: Duration::from_secs(3),
         wgpu_features: WgpuFeatures::default(),
-        wgpu_ctx: Some(wgpu_ctx.clone()),
+        wgpu_ctx: Some(get_wgpu_ctx()),
         load_system_fonts: false,
     })
     .unwrap();
     renderer
+}
+
+fn read_rgba_texture(texture: &wgpu::Texture) -> bytes::Bytes {
+    let (device, queue) = get_wgpu_ctx();
+    let buffer = new_download_buffer(&device, texture);
+
+    let mut encoder = device.create_command_encoder(&Default::default());
+    copy_to_buffer(&mut encoder, texture, &buffer);
+    queue.submit(Some(encoder.finish()));
+
+    download_buffer(&device, texture.size(), &buffer)
+}
+
+fn new_download_buffer(device: &wgpu::Device, texture: &wgpu::Texture) -> wgpu::Buffer {
+    let size = texture.size();
+    let block_size = texture.format().block_copy_size(None).unwrap();
+
+    device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("texture buffer"),
+        mapped_at_creation: false,
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+        size: (pad_to_256(block_size * size.width) * size.height) as u64,
+    })
+}
+
+fn copy_to_buffer(
+    encoder: &mut wgpu::CommandEncoder,
+    texture: &wgpu::Texture,
+    buffer: &wgpu::Buffer,
+) {
+    let size = texture.size();
+    let block_size = texture.format().block_copy_size(None).unwrap();
+    encoder.copy_texture_to_buffer(
+        wgpu::ImageCopyTexture {
+            aspect: wgpu::TextureAspect::All,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            texture,
+        },
+        wgpu::ImageCopyBuffer {
+            buffer,
+            layout: wgpu::ImageDataLayout {
+                bytes_per_row: Some(pad_to_256(size.width * block_size)),
+                rows_per_image: Some(size.height),
+                offset: 0,
+            },
+        },
+        size,
+    );
+}
+
+fn download_buffer(
+    device: &wgpu::Device,
+    size: wgpu::Extent3d,
+    source: &wgpu::Buffer,
+) -> bytes::Bytes {
+    let buffer = bytes::BytesMut::with_capacity((size.width * size.height * 4) as usize);
+    let (s, r) = bounded(1);
+    source
+        .slice(..)
+        .map_async(wgpu::MapMode::Read, move |result| {
+            if let Err(err) = s.send(result) {
+                error!("channel send error: {err}")
+            }
+        });
+
+    device.poll(wgpu::MaintainBase::Wait);
+
+    r.recv().unwrap().unwrap();
+    let mut buffer = buffer.writer();
+    {
+        let range = source.slice(..).get_mapped_range();
+        let chunks = range.chunks(pad_to_256(size.width * 4) as usize);
+        for chunk in chunks {
+            buffer
+                .write_all(&chunk[..(size.width * 4) as usize])
+                .unwrap();
+        }
+    };
+    source.unmap();
+    buffer.into_inner().into()
+}
+
+fn pad_to_256(value: u32) -> u32 {
+    if value % 256 == 0 {
+        value
+    } else {
+        value + (256 - (value % 256))
+    }
 }
