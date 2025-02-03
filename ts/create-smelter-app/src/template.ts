0fa6ced5ffa90d11e239bef2fd74fb644a3f858e
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-02-03 12:36:10 +0100

    [ts-sdk] Fix create-smelter-app after switch to pnpm (#944)

--- ts/create-smelter-app/src/template.ts
+++ ts/create-smelter-app/src/template.ts
@@ -16,8 +16,39 @@ export async function applyTemplate(
 
   const packageJsonPath = path.join(destination, 'package.json');
   const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
+  const transformedPackageJson = transformPackageJson(packageJson, projectName);
+  await fs.writeFile(
+    packageJsonPath,
+    JSON.stringify(transformedPackageJson, null, 2) + '\n',
+    'utf8'
+  );
+}
+
+export function transformPackageJson(packageJson: any, projectName: string): any {
   delete packageJson?.scripts?.['start'];
   delete packageJson['private'];
   packageJson.name = projectName;
-  await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n', 'utf8');
+  const LABEL = 'workspace:';
+
+  for (const dep of Object.keys((packageJson['dependencies'] as any) ?? {})) {
+    const depValue: string = packageJson?.['dependencies']?.[dep];
+    if (depValue && depValue.startsWith(LABEL)) {
+      packageJson['dependencies'][dep] = depValue.substring(LABEL.length);
+    }
+  }
+
+  for (const dep of Object.keys((packageJson['devDependencies'] as any) ?? {})) {
+    const depValue: string = packageJson?.['devDependencies']?.[dep];
+    if (depValue && depValue.startsWith(LABEL)) {
+      packageJson['devDependencies'][dep] = depValue.substring(LABEL.length);
+    }
+  }
+
+  for (const dep of Object.keys((packageJson['peerDependencies'] as any) ?? {})) {
+    const depValue: string = packageJson?.['peerDependencies']?.[dep];
+    if (depValue && depValue.startsWith(LABEL)) {
+      packageJson['peerDependencies'][dep] = depValue.substring(LABEL.length);
+    }
+  }
+  return packageJson;
 }
